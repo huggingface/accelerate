18e3e6d7556e5459b527585b0c61f87b3497931a
Sylvain Gugger <sylvain.gugger@gmail.com>
2021-03-05 16:45:56 -0500

    Release checklist

--- setup.py
+++ setup.py
@@ -53,3 +53,22 @@ setup(
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
 )
+
+# Release checklist
+# 1. Change the version in __init__.py, setup.py as well as docs/source/conf.py.
+# 2. Commit these changes with the message: "Release: VERSION"
+# 3. Add a tag in git to mark the release: "git tag VERSION -m 'Adds tag VERSION for pypi' "
+#    Push the tag to git: git push --tags origin master
+# 4. Run the following commands in the top-level directory:
+#      python setup.py bdist_wheel
+#      python setup.py sdist
+# 5. Upload the package to the pypi test server first:
+#      twine upload dist/* -r pypitest
+#      twine upload dist/* -r pypitest --repository-url=https://test.pypi.org/legacy/
+# 6. Check that you can install it in a virtualenv by running:
+#      pip install -i https://testpypi.python.org/pypi accelerate
+# 7. Upload the final version to actual pypi:
+#      twine upload dist/* -r pypi
+# 8. Add release notes to RELEASE.md and the tag in github once everything is looking hunky-dory.
+# 9. Add the release version to docs/source/_static/js/custom.js and .circleci/deploy.sh
+# 10. Update the version in __init__.py, setup.py to the new version "-dev" and push to master
