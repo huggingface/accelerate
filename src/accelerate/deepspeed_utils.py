e14f0f5102b094525d510aa03eeb354cd2fab839
Vasudev <7vasudevgupta@gmail.com>
2021-05-28 02:18:02 +0530

    make quality

--- src/accelerate/deepspeed_utils.py
+++ src/accelerate/deepspeed_utils.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
 from .optimizer import AcceleratedOptimizer
 from .state import is_apex_available, is_deepspeed_available
 
