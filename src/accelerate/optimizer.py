b9c9a70e68b1daa2b325f178b35f0c43e9f5f312
Peter Melchior <peter.m.melchior@gmail.com>
2021-06-14 23:56:13 -0400

    added closure argument to optimizer.step()

--- src/accelerate/optimizer.py
+++ src/accelerate/optimizer.py
@@ -102,14 +102,14 @@ class AcceleratedOptimizer(torch.optim.Optimizer):
                 set_to_none = False
             self.optimizer.zero_grad(set_to_none=set_to_none)
 
-    def step(self):
+    def step(self, closure=None):
         if self.state.distributed_type == DistributedType.TPU:
-            xm.optimizer_step(self.optimizer)
+            xm.optimizer_step(self.optimizer, dict(closure=closure))
         elif self.scaler is not None:
-            self.scaler.step(self.optimizer)
+            self.scaler.step(self.optimizer, closure)
             self.scaler.update()
         else:
-            self.optimizer.step()
+            self.optimizer.step(closure)
 
     def _switch_parameters(self, parameters_map):
         for param_group in self.optimizer.param_groups:
