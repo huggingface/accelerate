19ec4a782c9626766498e86049fdecf7c318ad08
Sylvain Gugger <sylvain.gugger@gmail.com>
2021-09-27 11:04:29 -0400

    Release v0.5.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ extras["sagemaker"] = [
 
 setup(
     name="accelerate",
-    version="0.6.0.dev0",
+    version="0.5.1",
     description="Accelerate",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
