b23fe274ad30c7e452985612092849dea3c19720
Iker <igarciaf896@gmail.com>
2022-02-03 15:53:34 +0100

    Remove dtype verbosity

--- src/accelerate/test_utils/training.py
+++ src/accelerate/test_utils/training.py
@@ -36,6 +36,10 @@ class RegressionModel(torch.nn.Module):
         super().__init__()
         self.a = torch.nn.Parameter(torch.tensor(a).float())
         self.b = torch.nn.Parameter(torch.tensor(b).float())
+        self.first_batch = True
 
     def forward(self, x=None):
+        if self.first_batch:
+            print(f"Model dtype: {self.a.dtype}, {self.b.dtype}. Input dtype: {x.dtype}")
+            self.first_batch = False
         return x * self.a + self.b
