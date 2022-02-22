d742bce5254a1cc4148841eb9d7b7f92a6c7e897
Zhiyuan Chen <this@zyc.ai>
2022-02-23 04:28:54 +0800

    add support of gather_object (#238)
    * add support of gather_object
    
    fixes #235
    
    * Update src/accelerate/utils.py
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    * Update src/accelerate/utils.py
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    * skip error_on_other_type
    
    * fix picklable object described as any object
    
    * do not concat gathered objects
    
    * Update utils.py
    
    * format code
    
    * Update src/accelerate/utils.py
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    * Update src/accelerate/utils.py
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/accelerate/utils.py
+++ src/accelerate/utils.py
@@ -18,7 +18,7 @@ import random
 from collections.abc import Mapping
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import List, Optional, Union
+from typing import Any, List, Optional, Union
 
 import numpy as np
 import torch
@@ -348,6 +348,39 @@ def gather(tensor):
         return _cpu_gather(tensor)
     else:
         return tensor
+
+
+def _gpu_gather_object(object: Any):
+    def _gpu_gather_object_one(object: Any):
+        output_objects = [None for _ in range(AcceleratorState().num_processes)]
+        torch.distributed.all_gather_object(output_objects, object)
+        return output_objects
+
+    return recursively_apply(_gpu_gather_object_one, object)
+
+
+_cpu_gather_object = _gpu_gather_object
+
+
+def gather_object(object: Any):
+    """
+    Recursively gather object in a nested list/tuple/dictionary of objects from all devices.
+
+    Args:
+        object (nested list/tuple/dictionary of picklable object):
+            The data to gather.
+
+    Returns:
+        The same data structure as :obj:`object` with all the objects sent to every device.
+    """
+    if AcceleratorState().distributed_type == DistributedType.TPU:
+        raise NotImplementedError("gather objects in TPU is not supported")
+    elif AcceleratorState().distributed_type == DistributedType.MULTI_GPU:
+        return _gpu_gather_object(object)
+    elif AcceleratorState().distributed_type == DistributedType.MULTI_CPU:
+        return _cpu_gather_object(object)
+    else:
+        return object
 
 
 def _gpu_broadcast(data, src=0):
