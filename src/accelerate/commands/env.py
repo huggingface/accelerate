13bc1b92f9dd2b9b4d721002cce4826d9ae223d4
João Gustavo Atkinson Amorim <joaogustavoamorim@gmail.com>
2022-03-14 11:37:02 -0300

    add default configs of accelerate

--- src/accelerate/commands/env.py
+++ src/accelerate/commands/env.py
@@ -1,10 +1,12 @@
 import argparse
+import os
 import platform
 
 import numpy as np
 import torch
 
 from accelerate import __version__ as version
+from accelerate.commands.config import default_config_file, load_config_from_file
 
 
 def env_command_parser(subparsers=None):
@@ -12,6 +14,10 @@ def env_command_parser(subparsers=None):
         parser = subparsers.add_parser("env")
     else:
         parser = argparse.ArgumentParser("Accelerate env command")
+
+    parser.add_argument(
+        "--config_file", default=None, help="The config file to use for the default values in the launching script."
+    )
 
     if subparsers is not None:
         parser.set_defaults(func=env_command)
@@ -22,8 +28,13 @@ def env_command(args):
     pt_version = torch.__version__
     pt_cuda_available = torch.cuda.is_available()
 
+    accelerate_config = "Not found"
+    # Get the default from the config file.
+    if args.config_file is not None or os.path.isfile(default_config_file):
+        accelerate_config = load_config_from_file(args.config_file).to_dict()
+
     info = {
-        "`accelerate` version": version,
+        "`Accelerate` version": version,
         "Platform": platform.platform(),
         "Python version": platform.python_version(),
         "Numpy version": np.__version__,
@@ -31,15 +42,26 @@ def env_command(args):
     }
 
     print("\nCopy-and-paste the text below in your GitHub issue\n")
-    print("\n".join([f"- {prop}: {val}" for prop, val in info.items()]) + "\n")
+    print("\n".join([f"- {prop}: {val}" for prop, val in info.items()]))
+
+    print("- `Accelerate` configs:")
+    accelerate_config_str = (
+        "\n".join([f"\t- {prop}: {val}" for prop, val in accelerate_config.items()])
+        if isinstance(accelerate_config, dict)
+        else f"\t{accelerate_config}"
+    )
+    print(accelerate_config_str)
+
+    info["`Accelerate` configs"] = accelerate_config
 
     return info
 
 
-def main():
+def main() -> int:
     parser = env_command_parser()
     args = parser.parse_args()
     env_command(args)
+    return 0
 
 
 if __name__ == "__main__":
