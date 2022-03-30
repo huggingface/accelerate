b8727b52f841e65b1368959b22afe5838c5eef96
Sylvain Gugger <Sylvain.gugger@gmail.com>
2022-03-30 17:08:19 -0400

    Style post comments

--- src/accelerate/scheduler.py
+++ src/accelerate/scheduler.py
@@ -24,9 +24,9 @@ class AcceleratedScheduler:
     - step was skipped because of gradient accumulation
 
     Args:
-        scheduler (`torch.optim.lr_scheduler._LRScheduler`): 
+        scheduler (`torch.optim.lr_scheduler._LRScheduler`):
             The scheduler to wrap.
-        optimizers (one or a list of `torch.optim.Optimizer`): 
+        optimizers (one or a list of `torch.optim.Optimizer`):
             The optimizers used.
         step_with_optimizer (`bool`, *optional*, defaults to `True`):
             Whether or not the scheduler should be stepped at each optimizer step.
