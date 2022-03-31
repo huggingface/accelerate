a5b8811bfa63d3d01ad0f897de6db0a7669eeb8d
Sylvain Gugger <Sylvain.gugger@gmail.com>
2022-03-31 09:24:08 -0400

    Release: v0.6.2

--- src/accelerate/__init__.py
+++ src/accelerate/__init__.py
@@ -2,7 +2,7 @@
 # There's no way to ignore "F401 '...' imported but unused" warnings in this
 # module, but to preserve other warnings. So, don't check this module at all.
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 
 from .accelerator import Accelerator
 from .kwargs_handlers import DistributedDataParallelKwargs, GradScalerKwargs, InitProcessGroupKwargs
