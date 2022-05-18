62878871fe0ea67bddce48ba9b635050647b71f3
muellerzr <muellerzr@gmail.com>
2022-05-18 13:20:30 -0400

    Nits from code review

--- src/accelerate/utils/other.py
+++ src/accelerate/utils/other.py
@@ -124,8 +124,8 @@ def write_basic_config(mixed_precision="no", save_location: str = default_json_c
             Mixed Precision to use. Should be one of "no", "fp16", or "bf16"
         save_location (`str`, *optional*, defaults to `default_json_config_file`):
             Optional custom save location. Should be passed to `--config_file` when using `accelerate launch`. Default
-            location is inside the huggingface cache folder as default_config.yaml, can be overriden by setting the
-            `HF_HOME` environmental variable.
+            location is inside the huggingface cache folder (`~/.cache/huggingface`) but can be overriden by setting the
+            `HF_HOME` environmental variable, followed by `accelerate/default_config.yaml`.
     """
     path = Path(save_location)
     path.parent.mkdir(parents=True, exist_ok=True)
