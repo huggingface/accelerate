c3165fa6c7260c8b5a9006af9e16dc75f3782d75
muellerzr <muellerzr@gmail.com>
2022-05-18 15:05:20 -0400

    Autoimport err

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from msilib.schema import Feature
 import os
 import shutil
 import subprocess
