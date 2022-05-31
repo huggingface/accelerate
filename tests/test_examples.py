61703f9086062cf92a0745b473a71b99954cf17c
muellerzr <muellerzr@gmail.com>
2022-05-31 13:05:42 -0400

    Use step 6

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -147,7 +147,7 @@ class FeatureExamplesTests(TempDirTestCase):
         --output_dir {self.tmpdir}
         """.split()
         _ = subprocess.run(self._launch_args + testargs, stdout=subprocess.PIPE, env=os.environ)
-        self.assertTrue(os.path.exists(os.path.join(self.tmpdir, "step_4")))
+        self.assertTrue(os.path.exists(os.path.join(self.tmpdir, "step_6")))
 
     def test_load_states_by_epoch(self):
         testargs = f"""
@@ -164,7 +164,7 @@ class FeatureExamplesTests(TempDirTestCase):
     def test_load_states_by_steps(self):
         testargs = f"""
         examples/by_feature/checkpointing.py
-        --resume_from_checkpoint {os.path.join(self.tmpdir, "step_4")}
+        --resume_from_checkpoint {os.path.join(self.tmpdir, "step_6")}
         """.split()
         output = subprocess.run(
             self._launch_args + testargs, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
