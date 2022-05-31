d23075664b88428c200b4220deb902292b18dae0
Sylvain Gugger <Sylvain.gugger@gmail.com>
2022-05-31 16:58:30 -0400

    Protobouf :poop:

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ extras["test"] = [
     "scipy",
     "sklearn"
 ]
-extras["test_trackers"] = ["wandb", "comet-ml", "tensorflow>=2.6.2", "tensorboard"]
+extras["test_trackers"] = ["wandb", "comet-ml", "protobuf<=3.20.1", "tensorflow>=2.6.2", "tensorboard"]
 extras["dev"] = extras["quality"] + extras["test"]
 
 extras["sagemaker"] = [
