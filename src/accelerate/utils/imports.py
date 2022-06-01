9beefc6464b98cec1e7dca8a81cbc34e2a994688
muellerzr <muellerzr@gmail.com>
2022-06-01 15:16:18 -0400

    Keep is tb available

--- src/accelerate/utils/imports.py
+++ src/accelerate/utils/imports.py
@@ -63,6 +63,10 @@ def is_deepspeed_available():
             return False
 
 
+def is_tensorboard_available():
+    return importlib.util.find_spec("tensorboard") is not None or importlib.util.find_spec("tensorboardX") is not None
+
+
 def is_wandb_available():
     return importlib.util.find_spec("wandb") is not None
 
