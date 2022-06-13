9693814751f848b3198599505247c8011c7af06e
muellerzr <muellerzr@gmail.com>
2022-06-13 16:56:42 -0400

    mp_fn

--- tests/xla_spawn.py
+++ tests/xla_spawn.py
@@ -78,11 +78,7 @@ def main():
 
     # Patch sys.argv
     sys.argv = [args.training_script] + args.training_script_args + ["--tpu_num_cores", str(args.num_cores)]
-    xmp.spawn(mod.__main__, args=(), nprocs=args.num_cores)
-
-def _mp_fn(index):
-    # For xla_spawn (TPUs)
-    main()
+    xmp.spawn(mod._mp_fn, args=(), nprocs=args.num_cores)
 
 if __name__ == "__main__":
     main()
