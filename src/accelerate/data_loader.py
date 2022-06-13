534a7f6f8f4a368fac6b07f202bc32299eb78a38
muellerzr <muellerzr@gmail.com>
2022-06-13 12:13:39 -0400

    Flip num_processes logic

--- src/accelerate/data_loader.py
+++ src/accelerate/data_loader.py
@@ -301,7 +301,7 @@ class DataLoaderShard(DataLoader):
             synchronize_rng_states(self.rng_types, self.generator)
         state = AcceleratorState()
         for batch in super().__iter__():
-            if state.distributed_type == DistributedType.TPU and state.num_processes > 1:
+            if state.distributed_type == DistributedType.TPU and state.num_processes < 2:
                 xm.mark_step()
             yield batch if self.device is None else send_to_device(batch, self.device)
 
@@ -410,7 +410,7 @@ class DataLoaderDispatcher(DataLoader):
 
             data_slice = slice(state.process_index * batch_size, (state.process_index + 1) * batch_size)
 
-            if state.distributed_type == DistributedType.TPU and state.num_processes > 1:
+            if state.distributed_type == DistributedType.TPU and state.num_processes < 2:
                 xm.mark_step()
             yield slice_tensors(batch, data_slice)
 
