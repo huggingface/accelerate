b5d0ebc693aad74ae47436575461ae5ae124aee5
muellerzr <muellerzr@gmail.com>
2022-06-29 14:02:32 -0400

    Add more checks?

--- src/accelerate/optimizer.py
+++ src/accelerate/optimizer.py
@@ -102,6 +102,7 @@ class AcceleratedOptimizer(torch.optim.Optimizer):
 
     def zero_grad(self, set_to_none=None):
         print(f'Should zero_grad (from optimizer): {self.accelerator_state.sync_gradients}\n')
+        print(f'State ID from Optimizer: {id(AcceleratorState)}')
         if self.accelerator_state.sync_gradients:
             if is_torch_version("<", "1.7.0"):
                 if set_to_none is not None:
@@ -123,6 +124,7 @@ class AcceleratedOptimizer(torch.optim.Optimizer):
 
     def step(self, closure=None):
         print(f'Should step (from optimizer): {self.accelerator_state.sync_gradients}')
+        print(f'State ID from Optimizer: {id(AcceleratorState)}')
         if self.accelerator_state.sync_gradients:
             if self.accelerator_state.distributed_type == DistributedType.TPU:
                 optimizer_args = {"closure": closure} if closure is not None else {}
--- src/accelerate/test_utils/scripts/test_sync.py
+++ src/accelerate/test_utils/scripts/test_sync.py
@@ -193,9 +193,9 @@ def test_gradient_accumulation_with_opt_and_scheduler():
             accelerator.print(f'Iteration: {iteration}\nState: {accelerator.state}\n')
             step_model(ddp_model, ddp_input, ddp_target, accelerator)
             accelerator.print(f'Should sync (from test): {accelerator.state.sync_gradients}')
-        ddp_opt.step()
-        ddp_sched.step()
-        ddp_opt.zero_grad()
+            ddp_opt.step()
+            ddp_sched.step()
+            ddp_opt.zero_grad()
 
         # DDP model and model should only be in sync when not (iteration % 2 == 0)
         for param, ddp_param in zip(model.parameters(), ddp_model.parameters()):
