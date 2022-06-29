fdd239021eabfa8f8f49558ff79d61acc5947472
muellerzr <muellerzr@gmail.com>
2022-06-29 15:04:02 -0400

    Include seed

--- src/accelerate/test_utils/scripts/test_sync.py
+++ src/accelerate/test_utils/scripts/test_sync.py
@@ -179,6 +179,7 @@ def test_gradient_accumulation_with_opt_and_scheduler():
     accelerator = Accelerator(gradient_accumulation_steps=2)
     # Test that context manager behaves properly
     model, opt, sched, ddp_model, ddp_opt, ddp_sched, ddp_input, ddp_target = get_training_setup(accelerator, True)
+    set_seed(42)
     for iteration in range(4):
         # Gather the distributed inputs and targs for the base model
         input, target = accelerator.gather((ddp_input, ddp_target))
