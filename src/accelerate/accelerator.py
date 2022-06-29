2462c56cef20e0f11dc4cbb1ce51151a7b0b2b5b
muellerzr <muellerzr@gmail.com>
2022-06-29 13:00:28 -0400

    More elif

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -383,8 +383,10 @@ class Accelerator:
         "Checks if self.step % self.gradient_accumulation_steps == 0 or step == length of dataloader"
         if self.gradient_accumulation_steps == 1:
             return True
+        elif self.step == 0: 
+            return False
         self.print(f'Step: {self.step}, N_Steps: {self.gradient_accumulation_steps}\nShould sync: {(self.step % self.gradient_accumulation_steps) == 0}')
-        if ((self.step % self.gradient_accumulation_steps) == 0) or (self.step + 1 == len(dataloader)):
+        if (self.step % self.gradient_accumulation_steps) != 0 or (self.step + 1) == len(dataloader):
             return True
         return False
 
