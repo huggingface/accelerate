6d0e96f72842d53972abb2d755481361bbabb02c
Zach Mueller <muellerzr@gmail.com>
2022-07-11 16:39:39 +0200

    Print conditional checks

--- src/accelerate/test_utils/scripts/test_sync.py
+++ src/accelerate/test_utils/scripts/test_sync.py
@@ -223,6 +223,8 @@ def test_gradient_accumulation_with_opt_and_scheduler(split_batches=False, dispa
         did_step = (((iteration + 1) % 2) == 0) or (iteration == (len(dataloader) - 1))
         if accelerator.num_processes > 1:
             print(f'Did we step at iteration {iteration}: {did_step}')
+            print(f'First conditional check: {((iteration+1)%2 == 0)}')
+            print(f'Second conditional check: {iteration == (len(dataloader) - 1)}')
             check_model_parameters(model, ddp_model, did_step, iteration)
         # Shuffle ddp_input on each iteration
         torch.manual_seed(1337 + iteration)
