de6ac00e00ba6c488a95577f77fb0e7ef749ef55
Zach Mueller <muellerzr@gmail.com>
2022-07-21 07:30:10 -0400

    Fix doc

--- examples/by_feature/multi_process_metrics.py
+++ examples/by_feature/multi_process_metrics.py
@@ -187,8 +187,8 @@ def training_function(config, args):
                 else:
                     # Otherwise we add the number of samples seen
                     samples_seen += references.shape[0]
-            # All of this can be avoided if you use `Accelerate.gather_for_metrics` instead of `Accelerate.gather`:
-            # accelerate.gather_for_metrics((predictions, references), eval_dataloader)
+            # All of this can be avoided if you use `Accelerator.gather_for_metrics` instead of `Accelerator.gather`:
+            # accelerator.gather_for_metrics((predictions, references), eval_dataloader)
             metric.add_batch(
                 predictions=predictions,
                 references=references,
