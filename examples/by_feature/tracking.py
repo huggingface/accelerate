63682abc5026223216f9de0f77ef9cb0c1957a74
Zach Mueller <muellerzr@gmail.com>
2022-07-21 16:04:53 -0400

    Rename all the caches

--- examples/by_feature/tracking.py
+++ examples/by_feature/tracking.py
@@ -220,7 +220,6 @@ def training_function(config, args):
                     "train_loss": total_loss.item() / len(train_dataloader),
                     "epoch": epoch,
                 },
-                step=epoch,
             )
 
     # New Code #
