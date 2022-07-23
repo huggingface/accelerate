bdf7d842da99bc2940830414b0c2a8b13b0d42b9
Zach Mueller <muellerzr@gmail.com>
2022-07-23 00:08:02 +0000

    Fin

--- examples/nlp_example.py
+++ examples/nlp_example.py
@@ -143,7 +143,6 @@ def training_function(config, args):
         for step, batch in enumerate(train_dataloader):
             # We could avoid this line since we set the accelerator with `device_placement=True`.
             batch.to(accelerator.device)
-            batch = batch[:1]
             outputs = model(**batch)
             loss = outputs.loss
             loss = loss / gradient_accumulation_steps
