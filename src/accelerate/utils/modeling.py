f438a813ff0ac45cf4dc455fb10670f71c5ce373
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2022-07-27 09:46:12 -0400

    Fix `set_module_tensor_to_device` (#576)
    * Fix
    
    * Refine test
    
    * Fix test

--- src/accelerate/utils/modeling.py
+++ src/accelerate/utils/modeling.py
@@ -127,13 +127,11 @@ def set_module_tensor_to_device(
 
         if is_buffer:
             module._buffers[tensor_name] = new_value
-        elif module._parameters[tensor_name].device.type != "cuda":
+        elif torch.device(device) != module._parameters[tensor_name].device:
             param_cls = type(module._parameters[tensor_name])
             kwargs = module._parameters[tensor_name].__dict__
             new_value = param_cls(new_value, requires_grad=old_value.requires_grad, **kwargs).to(device)
             module._parameters[tensor_name] = new_value
-        else:
-            module._parameters[tensor_name] = module._parameters[tensor_name].to(device)
 
 
 def named_module_tensors(module: nn.Module, include_buffers: bool = True, recurse: bool = False):
