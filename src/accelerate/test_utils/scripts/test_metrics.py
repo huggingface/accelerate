f33bc8c70b54e8bd6dad4c74ac6262d6c3d9afaf
Zach Mueller <muellerzr@gmail.com>
2022-08-02 16:46:36 +0000

    All passing!

--- src/accelerate/test_utils/scripts/test_metrics.py
+++ src/accelerate/test_utils/scripts/test_metrics.py
@@ -40,7 +40,7 @@ def get_basic_setup(accelerator, num_samples=82):
     return model, ddp_model, dataloader
 
 
-def get_dataloader(accelerator: Accelerator, drop_last=False, use_longest=False):
+def get_dataloader(accelerator: Accelerator, use_longest=False):
     tokenizer = AutoTokenizer.from_pretrained("hf-internal-testing/mrpc-bert-base-cased")
     dataset = load_dataset("glue", "mrpc", split="validation")
 
@@ -62,12 +62,12 @@ def get_dataloader(accelerator: Accelerator, drop_last=False, use_longest=False)
             return tokenizer.pad(examples, padding="longest", return_tensors="pt")
         return tokenizer.pad(examples, padding="max_length", max_length=128, return_tensors="pt")
 
-    return DataLoader(tokenized_datasets, shuffle=False, collate_fn=collate_fn, batch_size=16, drop_last=drop_last)
+    return DataLoader(tokenized_datasets, shuffle=False, collate_fn=collate_fn, batch_size=16)
 
 
-def get_mrpc_setup(dispatch_batches, split_batches, drop_last):
+def get_mrpc_setup(dispatch_batches, split_batches):
     accelerator = Accelerator(dispatch_batches=dispatch_batches, split_batches=split_batches)
-    dataloader = get_dataloader(accelerator, drop_last, not dispatch_batches)
+    dataloader = get_dataloader(accelerator, not dispatch_batches)
     model = AutoModelForSequenceClassification.from_pretrained(
         "hf-internal-testing/mrpc-bert-base-cased", return_dict=True
     )
@@ -80,34 +80,30 @@ def generate_predictions(model, dataloader, accelerator):
     for batch in dataloader:
         input, target = batch.values()
         with torch.no_grad():
-            logits = model(input)
-            logits, target = accelerator.gather_for_metrics((logits, target))
-            logits_and_targets.append((logits, target))
-    inps, targs = [], []
-    for (inp, targ) in logits_and_targets:
-        inps.append(inp)
+            logit = model(input)
+            logit, target = accelerator.gather_for_metrics((logit, target))
+            logits_and_targets.append((logit, target))
+    logits, targs = [], []
+    for (logit, targ) in logits_and_targets:
+        logits.append(logit)
         targs.append(targ)
-    inps, targs = torch.cat(inps), torch.cat(targs)
-    return inps, targs
+    logits, targs = torch.cat(logits), torch.cat(targs)
+    return logits, targs
 
 
 def test_torch_metrics(accelerator: Accelerator, num_samples=82, dispatch_batches=False, split_batches=False):
     model, ddp_model, dataloader = get_basic_setup(accelerator, num_samples)
-    inps, targs = generate_predictions(ddp_model, dataloader, accelerator)
-    if not len(inps) == num_samples:
-        print(f"Unexpected number of inputs:\n    Expected: {num_samples}\n    Actual: {len(inps)}")
+    logits, targs = generate_predictions(ddp_model, dataloader, accelerator)
+    assert (
+        len(logits) == num_samples
+    ), f"Unexpected number of inputs:\n    Expected: {num_samples}\n    Actual: {len(logits)}"
 
 
 def test_mrpc(dispatch_batches: bool = False, split_batches: bool = False):
-    drop_last = dispatch_batches or split_batches
     metric = evaluate.load("glue", "mrpc")
-    setup, accelerator = get_mrpc_setup(dispatch_batches, split_batches, drop_last)
+    setup, accelerator = get_mrpc_setup(dispatch_batches, split_batches)
     # First do baseline
-    if accelerator.is_local_main_process:
-        print("--- Baseline ---")
     model, dataloader, device = setup["no"]
-    if accelerator.is_local_main_process:
-        print(f"Len dl: {len(dataloader)}\nLen dset: {len(dataloader.dataset)}\n")
     model.to(device)
     model.eval()
     for batch in dataloader:
@@ -119,8 +115,6 @@ def test_mrpc(dispatch_batches: bool = False, split_batches: bool = False):
     baseline = metric.compute()
 
     # Then do distributed
-    if accelerator.is_local_main_process:
-        print("--- gather_for_metrics ---")
     model, dataloader, device = setup["ddp"]
     model.eval()
     for batch in dataloader:
@@ -133,10 +127,9 @@ def test_mrpc(dispatch_batches: bool = False, split_batches: bool = False):
     distributed = metric.compute()
 
     for key in "accuracy f1".split():
-        if not math.isclose(baseline[key], distributed[key]):
-            print(
-                f"Baseline and Distributed are not the same for key {key}:\n\tBaseline: {baseline[key]}\n\tDistributed: {distributed[key]}\n"
-            )
+        assert math.isclose(
+            baseline[key], distributed[key]
+        ), f"Baseline and Distributed are not the same for key {key}:\n\tBaseline: {baseline[key]}\n\tDistributed: {distributed[key]}\n"
 
 
 def main():
