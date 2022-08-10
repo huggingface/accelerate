b9ea85fbe705f6585e47d11ca1287050ead82ea1
Zach Mueller <muellerzr@gmail.com>
2022-08-10 20:50:21 +0000

    Fin!

--- src/accelerate/utils/rich.py
+++ src/accelerate/utils/rich.py
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# from contextlib import contextmanager
-
-# from rich import get_console
-
 from rich.traceback import install
 
 
