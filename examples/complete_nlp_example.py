36420f53f394ee2c818619f13f700551cb9a96c3
Atharva Ingle <atharvaaingle@gmail.com>
2022-08-20 16:38:22 +0530

    remove check for main process for trackers initialization (#643)
    * remove check for main process for trackers initialization
    
    * removed is_main_process check for trackers initialization

--- examples/complete_nlp_example.py
+++ examples/complete_nlp_example.py
@@ -75,7 +75,7 @@ def training_function(config, args):
     batch_size = int(config["batch_size"])
 
     # We need to initialize the trackers we use, and also store our configuration
-    if args.with_tracking and accelerator.is_main_process:
+    if args.with_tracking:
         run = os.path.split(__file__)[-1].split(".")[0]
         accelerator.init_trackers(run, config)
 
