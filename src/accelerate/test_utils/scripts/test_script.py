9037a1d07d267798b76c4d2ce0774e0b8cc41061
DESKTOP-RCJN1PO <muellerzr@gmail.com>
2022-09-02 16:21:09 -0400

    Wrong import

--- src/accelerate/test_utils/scripts/test_script.py
+++ src/accelerate/test_utils/scripts/test_script.py
@@ -20,8 +20,8 @@ from torch.utils.data import DataLoader
 from accelerate import Accelerator
 from accelerate.data_loader import prepare_data_loader
 from accelerate.state import AcceleratorState
-from accelerate.test_utils import RegressionDataset, RegressionModel, are_the_same_tensors, is_torch_version
-from accelerate.utils import DistributedType, gather, is_bf16_available, set_seed, synchronize_rng_states
+from accelerate.test_utils import RegressionDataset, RegressionModel, are_the_same_tensors
+from accelerate.utils import DistributedType, gather, is_bf16_available, set_seed, synchronize_rng_states, is_torch_version
 
 
 def init_state_check():
