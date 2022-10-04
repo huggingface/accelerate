0b5542d2b71f805142909bcc21266d4b598d374d
Stas Bekman <stas@stason.org>
2022-10-04 16:22:42 -0700

    improve doc, arg names

--- src/accelerate/utils/launch.py
+++ src/accelerate/utils/launch.py
@@ -52,12 +52,13 @@ def _filter_args(args):
     return new_args
 
 
-def env_var_path_add(env_var_name, new_path):
+def env_var_path_add(env_var_name, path_to_add):
     """
-    Extends path-based env variable with a new path
+    Extends path-based environment variable's value with a new path and returns the updated value. It's up to the
+    caller to set it in os.environ.
     """
     paths = [p for p in os.environ.get(env_var_name, "").split(":") if len(p) > 0]
-    paths.append(str(new_path))
+    paths.append(str(path_to_add))
     return ":".join(paths)
 
 
