50e3e52222f6ee6e04be978b90b3f6ec086834c9
DESKTOP-42S1K65 <muellerzr@gmail.com>
2022-10-10 07:23:41 +1000

    Add gpu_ids to SageMakerConfig though it should never be set

--- src/accelerate/commands/config/config_args.py
+++ src/accelerate/commands/config/config_args.py
@@ -165,6 +165,7 @@ class SageMakerConfig(BaseConfig):
     profile: Optional[str] = None
     region: str = "us-east-1"
     num_machines: int = 1
+    gpu_ids: str = "all"
     base_job_name: str = f"accelerate-sagemaker-{num_machines}"
     pytorch_version: str = SAGEMAKER_PYTORCH_VERSION
     transformers_version: str = SAGEMAKER_TRANSFORMERS_VERSION
