bffdead812e12bf1044cff91358b41d249779d9c
Zach Mueller <muellerzr@gmail.com>
2022-10-18 14:46:17 +0000

    Add CLI testing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,21 +12,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import torch
+import inspect
 import os
 import unittest
-from tempfile import TemporaryDirectory
-import inspect
-from accelerate.test_utils import execute_subprocess_async
+from pathlib import Path
+
+import torch
+
 import accelerate
+from accelerate.test_utils import execute_subprocess_async
 
 
 class AccelerateLauncherTester(unittest.TestCase):
+    """
+    Test case for verifying the `accelerate launch` CLI operates correctly.
+    If a `default_config.yaml` file is located in the cache it will temporarily move it
+    for the duration of the tests.
+    """
+
     mod_file = inspect.getfile(accelerate.test_utils)
-    test_file_path = os.path.sep.join(
-        mod_file.split(os.path.sep)[:-1] + ["scripts", "test_cli.py"]
-    )
+    test_file_path = os.path.sep.join(mod_file.split(os.path.sep)[:-1] + ["scripts", "test_cli.py"])
+
     base_cmd = ["accelerate", "launch"]
+    config_folder = Path.home() / ".cache/huggingface/accelerate"
+    config_file = "default_config.yaml"
+    config_path = config_folder / config_file
+    changed_path = config_folder / "_default_config.yaml"
+
+    test_config_path = Path("tests/test_configs")
+
+    @classmethod
+    def setUpClass(cls):
+        if cls.config_path.is_file():
+            cls.config_path.rename(cls.changed_path)
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.changed_path.is_file():
+            cls.changed_path.rename(cls.config_path)
 
     def test_no_config(self):
-        execute_subprocess_async(self.base_cmd + [self.test_file_path], env=os.environ.copy())
+        cmd = self.base_cmd
+        if torch.cuda.is_available() and (torch.cuda.device_count() > 1):
+            cmd += ["--multi_gpu"]
+        execute_subprocess_async(cmd + [self.test_file_path], env=os.environ.copy())
+
+    def test_config_compatibility(self):
+        for config in sorted(self.test_config_path.glob("**/*.yaml")):
+            with self.subTest(config_file=config):
+                execute_subprocess_async(
+                    self.base_cmd + ["--config_file", str(config), self.test_file_path], env=os.environ.copy()
+                )
---
+++ tests/test_configs/README.md
@@ -0,0 +1,2 @@
+This folder contains test configs for `accelerate config`. These should be generated for each major version
+and are written based on `accelerate config` and selecting the "No distributed training" option.
