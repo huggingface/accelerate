87a7e0783f7b428fafa72a9e64d36625ee22888f
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2022-10-19 21:32:11 +0200

    fix transformers tests (#777)

--- tests/test_modeling_utils.py
+++ tests/test_modeling_utils.py
@@ -376,6 +376,10 @@ class ModelingUtilsTester(unittest.TestCase):
         max_memory = get_balanced_memory(model, max_memory={0: 300, 1: 500})
         self.assertDictEqual({0: 215, 1: 500}, max_memory)
 
+        # Last device always get max memory to give more buffer, even if CPU is provided
+        max_memory = get_balanced_memory(model, max_memory={0: 300, "cpu": 1000})
+        self.assertDictEqual({0: 300, "cpu": 1000}, max_memory)
+
         # If we set a device to 0, it's not counted.
         max_memory = get_balanced_memory(model, max_memory={0: 0, 1: 300, 2: 300})
         self.assertDictEqual({0: 0, 1: 215, 2: 300}, max_memory)
