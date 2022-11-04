80420e52cbe37b1bc3c71ecf9d22a06f639ceae3
Chris-hughes10 <31883449+Chris-hughes10@users.noreply.github.com>
2022-11-04 09:09:27 +0000

    Add test for join context manager

--- src/accelerate/test_utils/scripts/test_distributed_data_loop.py
+++ src/accelerate/test_utils/scripts/test_distributed_data_loop.py
@@ -103,6 +103,30 @@ def test_can_disable_even_batches():
     )
 
 
+def test_can_join_uneven_inputs():
+    accelerator = create_accelerator(even_batches=False)
+
+    model = torch.nn.Linear(1, 1)
+    ddp_model = accelerator.prepare(model)
+
+    dl = create_dataloader(accelerator, dataset_size=3, batch_size=1)
+
+    batch_idxs = []
+    with accelerator.join_uneven_inputs([ddp_model]):
+        for batch_idx, batch in enumerate(dl):
+            output = ddp_model(batch[0].float())
+            loss = output.sum()
+            loss.backward()
+            batch_idxs.append(batch_idx)
+
+    accelerator.wait_for_everyone()
+
+    if accelerator.process_index == 0:
+        assert batch_idxs == [0, 1]
+    elif accelerator.process_index == 1:
+        assert batch_idxs == [0]
+
+
 if __name__ == "__main__":
     accelerator = create_accelerator()
 
@@ -111,3 +135,6 @@ if __name__ == "__main__":
 
     accelerator.print("Run tests with even_batches disabled")
     test_can_disable_even_batches()
+
+    accelerator.print("Test joining uneven inputs")
+    test_can_join_uneven_inputs()
