6bd28790c2464c303319b71bcd38bf12bf769f90
Zachary Mueller <muellerzr@gmail.com>
2022-12-07 09:34:58 -0500

    Fix conditional (#907)
    * Fix conditional
    
    * Into one if statement

--- src/accelerate/commands/launch.py
+++ src/accelerate/commands/launch.py
@@ -1006,7 +1006,7 @@ def launch_command(args):
         raise ValueError(
             "You can only use one of `--cpu`, `--multi_gpu`, `--mps`, `--tpu`, `--use_deepspeed`, `--use_fsdp` at a time."
         )
-    if args.multi_gpu and args.num_processes < 2:
+    if args.multi_gpu and (args.num_processes is not None) and (args.num_processes < 2):
         raise ValueError("You need to use at least 2 processes to use `--multi_gpu`.")
 
     defaults = None
