13deee85c75a2fc5f142e72f8f607fe9ca2efcf6
Sylvain Gugger <Sylvain.gugger@gmail.com>
2022-12-12 16:04:46 -0500

    Add more tests in debug

--- examples/debug.py
+++ examples/debug.py
@@ -1,16 +1,39 @@
-from accelerate.utils import convert_model
+from accelerate.utils import convert_model, set_seed
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import torch
 
+set_seed(42)
 model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased").to(0)
 tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
 
 
 inputs = tokenizer("Hello, my name is.", return_tensors="pt").to(0)
-outputs = model(**inputs)
+outputs = model(**inputs, labels=torch.tensor([0]).to(0))
 
-convert_model(model)
-new_outputs = model(**inputs)
+set_seed(42)
+new_model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased").to(0)
+convert_model(new_model)
+new_outputs = new_model(**inputs, labels=torch.tensor([0]).to(0))
 
-print(outputs, new_outputs)
-print(torch.allclose(outputs.logits, new_outputs.logits))
+print("Outputs comparison at 1e-6/1e-5/1e-4")
+print(torch.allclose(outputs.logits, new_outputs.logits, atol=1e-6))
+print(torch.allclose(outputs.logits, new_outputs.logits, atol=1e-5))
+print(torch.allclose(outputs.logits, new_outputs.logits, atol=1e-4))
+
+outputs.loss.backward()
+new_outputs.loss.backward()
+
+grad1 = model.bert.embeddings.word_embeddings.weight.grad
+grad2 = model.bert.embeddings.word_embeddings.weight.grad
+print("Embeddings gradients at 1e-6/1e-5/1e-4")
+print(torch.allclose(grad1, grad2, atol=1e-6))
+print(torch.allclose(grad1, grad2, atol=1e-5))
+print(torch.allclose(grad1, grad2, atol=1e-4))
+
+grad1 = getattr(model.bert.encoder.layer, "0").attention.output.LayerNorm.weight.grad
+grad2 = getattr(new_model.bert.encoder.layer, "0").attention.output.LayerNorm.layer_norm_weight.grad
+print("Layer norm gradients at 1e-6/1e-5/1e-4")
+print(torch.allclose(grad1, grad2, atol=1e-6))
+print(torch.allclose(grad1, grad2, atol=1e-5))
+print(torch.allclose(grad1, grad2, atol=1e-4))
+
