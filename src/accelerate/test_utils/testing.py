28053cd39378180e394c06ad5311d5921fe2ef4f
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2022-12-16 11:54:32 +0100

    Turns out you might not have access to "torch.backends.mps"

--- src/accelerate/test_utils/testing.py
+++ src/accelerate/test_utils/testing.py
@@ -92,7 +92,8 @@ def require_mps(test_case):
     Decorator marking a test that requires MPS backend. These tests are skipped when torch doesn't support `mps`
     backend.
     """
-    return unittest.skipUnless(torch.backends.mps.is_available(), "test requires a `mps` backend support in `torch`")(
+    is_mps_supported = hasattr(torch.backends, "mps") and torch.backends.mps.is_available()
+    return unittest.skipUnless(is_mps_supported, "test requires a `mps` backend support in `torch`")(
         test_case
     )
 
