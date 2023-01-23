d54b599136e8e0c71f982728d505fbbfe29669bc
Sylvain Gugger <Sylvain.gugger@gmail.com>
2023-01-23 16:22:16 -0500

    Add method in Accelerator and example

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -25,7 +25,7 @@ from typing import List, Optional, Union
 import torch
 
 from .checkpointing import load_accelerator_state, load_custom_state, save_accelerator_state, save_custom_state
-from .data_loader import DataLoaderDispatcher, prepare_data_loader
+from .data_loader import DataLoaderDispatcher, prepare_data_loader, skip_batches
 from .logging import get_logger
 from .optimizer import AcceleratedOptimizer
 from .scheduler import AcceleratedScheduler
@@ -1918,3 +1918,13 @@ class Accelerator:
             if optimizer.step_was_skipped:
                 return True
         return False
+
+    def skip_batches(self, dataloader, num_batches: int = 0):
+        """
+        Creates a new `torch.utils.data.DataLoader` that will efficiently skip the first `num_batches`.
+
+        Args:
+            dataloader (`torch.utils.data.DataLoader`): The data loader in which to skip batches.
+            num_batches (`int`, *optional*, defaults to 0): The number of batches to skip
+        """
+        return skip_batches(dataloader, num_batches=num_batches)
