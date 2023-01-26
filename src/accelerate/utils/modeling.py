0c429e35d16c52cdd0a5ae7b9506129050b15926
Charles I Niswander II <darf333@gmail.com>
2023-01-26 12:28:52 -0500

    Deepspeed param check
    On line 146, in set_module_tensor_to_device(), adding a check for deepspeed parameters in the kwargs object, and not passing them solved the error I was receiving regarding the ds parameters not being recognized by torch.nn.Parameter.__new__(). With my admittedly limited knowledge, it seemed to me that the kwargs are not necessary to pass in the case of using Deepspeed+ Accelerate, and this bears out since the model loaded fine with zero-3 cpu parameter and buffer offload on a single-GPU machine, and performed perfectly comprehensible inference outputs (slowly) using the GPU.
    
    The error, in my case, was occurring here as called from accelerator's dispatch_model().
    
    Please let me know if my thinking on this is in anyway wrong! This fix worked for me. 
    
     `transformers` version: 4.26.0
    - Platform: Linux-5.15.83.1-microsoft-standard-WSL2-x86_64-with-glibc2.35
    - Python version: 3.10.6
    - Huggingface_hub version: 0.11.1
    - PyTorch version (GPU?): 1.13.1+cu117 (True)
    - Tensorflow version (GPU?): not installed (NA)
    - Flax version (CPU?/GPU?/TPU?): not installed (NA)
    - Jax version: not installed
    - JaxLib version: not installed
    - Using GPU in script?: Yes
    - Using distributed or parallel set-up in script?: Yes and no (zero-3 on single machine)

--- src/accelerate/utils/modeling.py
+++ src/accelerate/utils/modeling.py
@@ -143,7 +143,12 @@ def set_module_tensor_to_device(
         elif value is not None or torch.device(device) != module._parameters[tensor_name].device:
             param_cls = type(module._parameters[tensor_name])
             kwargs = module._parameters[tensor_name].__dict__
-            new_value = param_cls(new_value, requires_grad=old_value.requires_grad, **kwargs).to(device)
+            if (kwargs.get("ds_tensor") is not None):
+                new_value = param_cls(
+                    new_value, requires_grad=old_value.requires_grad).to(device)
+            else:
+                new_value = param_cls(
+                    new_value, requires_grad=old_value.requires_grad, **kwargs).to(device)
             module._parameters[tensor_name] = new_value
 
 
