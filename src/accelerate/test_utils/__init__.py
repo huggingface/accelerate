f2c55a75c1691d657a32f8319381c3266027f622
Sylvain Gugger <Sylvain.gugger@gmail.com>
2023-02-07 13:08:53 -0500

    Puts back isort skip flag

--- src/accelerate/test_utils/__init__.py
+++ src/accelerate/test_utils/__init__.py
@@ -1,4 +1,3 @@
-from .scripts import test_script, test_sync
 from .testing import (
     are_the_same_tensors,
     execute_subprocess_async,
@@ -14,3 +13,6 @@ from .testing import (
     slow,
 )
 from .training import RegressionDataset, RegressionModel
+
+
+from .scripts import test_script, test_sync  # isort: skip
