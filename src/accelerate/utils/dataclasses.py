7cd3684e427eb4c9059b56a257318a0ffa66b68b
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2023-03-06 15:13:04 +0530

    fix 🐛

--- src/accelerate/utils/dataclasses.py
+++ src/accelerate/utils/dataclasses.py
@@ -354,26 +354,30 @@ class TorchDynamoPlugin(KwargsHandler):
     """
 
     backend: DynamoBackend = field(
-        default="no",
+        default=None,
         metadata={
             "help": "Possible options are 'no', 'eager', 'aot_eager', 'inductor', 'nvfuser', 'aot_nvfuser', "
             "'aot_cudagraphs', 'ofi', 'fx2trt', 'onnxrt', 'ipex'"
         },
     )
     mode: str = field(
-        default="default", metadata={"help": "Possible options are 'default', 'reduce-overhead' or 'max-autotune'"}
+        default=None, metadata={"help": "Possible options are 'default', 'reduce-overhead' or 'max-autotune'"}
     )
-    fullgraph: bool = field(default=False, metadata={"help": "Whether it is ok to break model into several subgraphs"})
-    dynamic: bool = field(default=False, metadata={"help": "Whether to use dynamic shape for tracing"})
+    fullgraph: bool = field(default=None, metadata={"help": "Whether it is ok to break model into several subgraphs"})
+    dynamic: bool = field(default=None, metadata={"help": "Whether to use dynamic shape for tracing"})
     options: Any = field(default=None, metadata={"help": "A dictionary of options to pass to the backend."})
     disable: bool = field(default=False, metadata={"help": "Turn torch.compile() into a no-op for testing"})
 
     def __post_init__(self):
         prefix = "ACCELERATE_DYNAMO_"
-        self.backend = DynamoBackend(os.environ.get(prefix + "BACKEND", self.backend).upper())
-        self.mode = os.environ.get(prefix + "MODE", self.mode)
-        self.fullgraph = strtobool(os.environ.get(prefix + "FULLGRAPH", str(self.fullgraph))) == 1
-        self.dynamic = strtobool(os.environ.get(prefix + "DYNAMIC", str(self.dynamic))) == 1
+        if self.backend is None:
+            self.backend = DynamoBackend(os.environ.get(prefix + "BACKEND", "no").upper())
+        if self.mode is None:
+            self.mode = os.environ.get(prefix + "MODE", "default")
+        if self.fullgraph is None:
+            self.fullgraph = strtobool(os.environ.get(prefix + "FULLGRAPH", "False")) == 1
+        if self.dynamic is None:
+            self.dynamic = strtobool(os.environ.get(prefix + "DYNAMIC", "False")) == 1
 
     def to_dict(self):
         dynamo_config = copy.deepcopy(self.__dict__)
