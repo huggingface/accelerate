f7e9fd34833e640ef0d628b5797021449da07a67
younesbelkada <younesbelkada@gmail.com>
2023-03-23 10:18:29 +0000

    fix bias issue

--- src/accelerate/utils/modeling.py
+++ src/accelerate/utils/modeling.py
@@ -164,18 +164,15 @@ def set_module_tensor_to_device(
                 new_value = param_cls(new_value, requires_grad=old_value.requires_grad).to(device)
             module._parameters[tensor_name] = new_value
 
-            if (
-                module.__class__.__name__ == "Linear8bitLt"
-                and getattr(module.weight, "SCB", None) is None
-                and module.bias is not None
-            ):
+            if module.__class__.__name__ == "Linear8bitLt" and getattr(module.weight, "SCB", None) is None:
                 # quantize only if necessary
-                if (
-                    module.bias.device.type != "meta"
-                    and not getattr(module.weight, "SCB", None)
-                    and isinstance(device, int)
-                ):
-                    module = module.cuda(device)
+                if not getattr(module.weight, "SCB", None) and isinstance(device, int):
+                    if module.bias is not None and module.bias.device.type != "meta":
+                        # if a bias exists, we need to wait until the bias is set on the correct device
+                        module = module.cuda(device)
+                    elif module.bias is None:
+                        # if no bias exists, we can quantize right away
+                        module = module.cuda(device)
 
 
 def named_module_tensors(module: nn.Module, include_buffers: bool = True, recurse: bool = False):
