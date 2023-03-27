e656c15c26a980400a769681cf47b9b4d36efa60
Wang, Yi A <yi.a.wang@intel.com>
2023-03-27 06:54:43 -0700

    fix test error
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -1275,7 +1275,7 @@ class Accelerator:
         if self.distributed_type == DistributedType.TPU and self.state.fork_launched:
             model = xmp.MpModelWrapper(model).to(self.device)
         # torch.compile should be called last.
-        if self.state.dynamo_plugin.backend != DynamoBackend.NO:
+        if self.state.dynamo_plugin and self.state.dynamo_plugin.backend != DynamoBackend.NO:
             if not hasattr(torch, "compile"):
                 raise ValueError("Using torch.compile requires PyTorch 2.0 or higher.")
             model = torch.compile(model, **self.state.dynamo_plugin.to_kwargs())
