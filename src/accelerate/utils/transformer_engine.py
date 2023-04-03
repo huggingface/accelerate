7b257a06b38eb3679212eb2f14693add47ecf9d9
Zach Mueller <muellerzr@gmail.com>
2023-04-03 09:24:37 -0400

    Raise import err

--- src/accelerate/utils/transformer_engine.py
+++ src/accelerate/utils/transformer_engine.py
@@ -72,6 +72,8 @@ def has_transformer_engine_layers(model):
     """
     Returns whether a given model has some `transformer_engine` layer or not.
     """
+    if not is_fp8_available():
+        raise ImportError("Using `has_transformer_engine_layers` requires transformer_engine to be installed.")
     for m in model.modules():
         if isinstance(m, (te.LayerNorm, te.Linear)):
             return True
