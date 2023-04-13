f2f810c536017e2e3d2f681721e44e6fb598a83a
Zachary Mueller <muellerzr@gmail.com>
2023-04-13 15:23:48 -0400

    Allow xpu backend (#1313)
    * Allow xpu set
    
    * Use in dataclass

--- src/accelerate/state.py
+++ src/accelerate/state.py
@@ -127,8 +127,8 @@ class PartialState:
             elif int(os.environ.get("LOCAL_RANK", -1)) != -1 and not cpu:
                 self.distributed_type = DistributedType.MULTI_GPU
                 if not torch.distributed.is_initialized():
-                    torch.distributed.init_process_group(backend="nccl", **kwargs)
-                    self.backend = "nccl"
+                    self.backend = kwargs.pop("backend")
+                    torch.distributed.init_process_group(backend=self.backend, **kwargs)
                 self.num_processes = torch.distributed.get_world_size()
                 self.process_index = torch.distributed.get_rank()
                 self.local_process_index = int(os.environ.get("LOCAL_RANK", -1))
