43e7229a1a107bbab11e41facf172377ba78fbb2
Zachary Mueller <muellerzr@gmail.com>
2023-04-17 13:58:53 -0400

    Add test flag and import check for dynamo (#1322)
    * Add is_dynamo_available + marker
    
    * Use min_torch_version instead

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -1277,8 +1277,8 @@ class Accelerator:
             model = xmp.MpModelWrapper(model).to(self.device)
         # torch.compile should be called last.
         if self.state.dynamo_plugin.backend != DynamoBackend.NO:
-            if not hasattr(torch, "compile"):
-                raise ValueError("Using torch.compile requires PyTorch 2.0 or higher.")
+            if not is_torch_version(">=", "2.0"):
+                raise ValueError("Using `torch.compile` requires PyTorch 2.0 or higher.")
             model = torch.compile(model, **self.state.dynamo_plugin.to_kwargs())
         return model
 
