fa438d61ce653efb53c342f8ffe0eab49c2c65d7
Zach Mueller <muellerzr@gmail.com>
2023-05-05 13:03:56 +0000

    Fix gather_obj

--- src/accelerate/utils/operations.py
+++ src/accelerate/utils/operations.py
@@ -250,12 +250,10 @@ def gather(tensor):
 
 
 def _gpu_gather_object(object: Any):
-    def _gpu_gather_object_one(object: Any):
-        output_objects = [None for _ in range(PartialState().num_processes)]
-        torch.distributed.all_gather_object(output_objects, object)
-        return output_objects
-
-    return recursively_apply(_gpu_gather_object_one, object)
+    output_objects = [None for _ in range(PartialState().num_processes)]
+    torch.distributed.all_gather_object(output_objects, object)
+    # all_gather_object returns a list of lists, so we need to flatten it
+    return [x for y in output_objects for x in y]
 
 
 _cpu_gather_object = _gpu_gather_object
