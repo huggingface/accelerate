a20e1fcc7661d59c45b17a43446903487fe9a8a8
Patrick von Platen <patrick.v.platen@gmail.com>
2023-05-16 16:21:54 +0100

    Apply suggestions from code review
    Co-authored-by: Zachary Mueller <muellerzr@gmail.com>

--- src/accelerate/utils/other.py
+++ src/accelerate/utils/other.py
@@ -33,7 +33,9 @@ if is_tpu_available(check_device=False):
 
 
 def is_compiled_module(module):
-    """Check whether the module was compiled with torch.compile()"""
+    """
+    Check whether the module was compiled with torch.compile()
+    """
     if is_torch_version("<", "2.0.0") or not hasattr(torch, "_dynamo"):
         return False
     return isinstance(module, torch._dynamo.eval_frame.OptimizedModule)
