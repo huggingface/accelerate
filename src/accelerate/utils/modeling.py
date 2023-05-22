bf3cd30a66d8753dd385b17f19dfa53352dc05e2
Tim Dettmers <TimDettmers@users.noreply.github.com>
2023-05-22 08:35:14 -0700

    4-bit QLoRA via bitsandbytes (4-bit base model + LoRA) (#1458)
    * Added change for FP4.
    
    * fix suggestion
    
    * better check
    
    ---------
    
    Co-authored-by: younesbelkada <younesbelkada@gmail.com>

--- src/accelerate/utils/modeling.py
+++ src/accelerate/utils/modeling.py
@@ -172,9 +172,9 @@ def set_module_tensor_to_device(
         elif value is not None or torch.device(device) != module._parameters[tensor_name].device:
             param_cls = type(module._parameters[tensor_name])
             kwargs = module._parameters[tensor_name].__dict__
-            if param_cls.__name__ == "Int8Params":
-                # downcast to fp16 if any
-                if new_value.dtype == torch.float32:
+            if param_cls.__name__ in ["Int8Params", "FP4Params"]:
+                if param_cls.__name__ == "Int8Params" and new_value.dtype == torch.float32:
+                    # downcast to fp16 if any - needed for 8bit serialization
                     new_value = new_value.to(torch.float16)
                 new_value = param_cls(new_value, requires_grad=old_value.requires_grad, **kwargs).to(device)
             else:
