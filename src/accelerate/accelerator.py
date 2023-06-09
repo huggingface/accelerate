665d5180fcc01d5700f7a9aa3f9bdb75c6055dce
Zach Mueller <muellerzr@gmail.com>
2023-06-09 13:10:53 -0400

    Check for bak and expand docs on directory structure (#1571)
    * Check for bak and expand doc
    
    * Better regex
    
    * Update docstring
    
    * Use exclusion at beginning and simplify check for digit

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -2461,7 +2461,8 @@ class Accelerator:
 
         <Tip>
 
-        Should only be used in conjunction with [`Accelerator.save_state`].
+        Should only be used in conjunction with [`Accelerator.save_state`]. If a file is not registered for
+        checkpointing, it will not be loaded if stored in the directory.
 
         </Tip>
 
@@ -2551,7 +2552,9 @@ class Accelerator:
             map_location,
             **load_model_func_kwargs,
         )
-        custom_checkpoints = [f for f in os.listdir(input_dir) if "custom_checkpoint" in f]
+        custom_checkpoints = [
+            f for f in os.listdir(input_dir) if re.search(r"^custom_checkpoint_\d+\.pkl$", f) is not None
+        ]
         if len(custom_checkpoints) != len(self._custom_objects):
             err = "Number of custom checkpoints in folder {input_dir} does not match the number of registered objects:"
             err += f"\n\tFound checkpoints: {len(custom_checkpoints)}"
