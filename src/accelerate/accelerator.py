543c59af224e3ea273633732319916b0698234ab
Zach Mueller <muellerzr@gmail.com>
2023-06-12 14:37:43 -0400

    Expand `prepare()` doc (#1580)
    * Expand device_placement
    
    * Expand doc
    
    * Update src/accelerate/accelerator.py
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    * Update accelerator.py
    
    ---------
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -1092,7 +1092,7 @@ class Accelerator:
 
             device_placement (`list[bool]`, *optional*):
                 Used to customize whether automatic device placement should be performed for each object passed. Needs
-                to be a list of the same length as `args`.
+                to be a list of the same length as `args`. Not compatible with DeepSpeed or FSDP.
 
         <Tip>
 
@@ -1100,7 +1100,7 @@ class Accelerator:
 
         </Tip>
 
-        Example:
+        Examples:
 
         ```python
         >>> from accelerate import Accelerator
@@ -1108,6 +1108,18 @@ class Accelerator:
         >>> accelerator = Accelerator()
         >>> # Assume a model, optimizer, data_loader and scheduler are defined
         >>> model, optimizer, data_loader, scheduler = accelerator.prepare(model, optimizer, data_loader, scheduler)
+        ```
+
+        ```python
+        >>> from accelerate import Accelerator
+
+        >>> accelerator = Accelerator()
+        >>> # Assume a model, optimizer, data_loader and scheduler are defined
+        >>> device_placement = [True, True, False, False]
+        >>> # Will place the first to items passed in automatically to the right device but not the last two.
+        >>> model, optimizer, data_loader, scheduler = accelerator.prepare(
+        ...     model, optimizer, data_loader, scheduler, device_placement=device_placement
+        ... )
         ```
         """
         if device_placement is None:
