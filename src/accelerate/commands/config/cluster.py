e60a4243988a636bda8a6bf99044fb313d5a9e0e
Abhilash Majumder <30946547+abhilash1910@users.noreply.github.com>
2023-06-15 22:41:24 +0530

    Remove asking xpu plugin for non xpu devices (#1594)
    * remove asking xpu plugin for non xpu devices
    
    * style

--- src/accelerate/commands/config/cluster.py
+++ src/accelerate/commands/config/cluster.py
@@ -110,7 +110,7 @@ def get_cluster_input():
             default=False,
             error_message="Please enter yes or no.",
         )
-    if not use_cpu and is_xpu_available():
+    if not use_cpu and is_xpu_available() and distributed_type not in [DistributedType.MULTI_GPU, DistributedType.TPU]:
         ipex_config["use_xpu"] = _ask_field(
             "Do you want to use XPU plugin to speed up training on XPU? [yes/NO]:",
             _convert_yes_no_to_bool,
