122e0cadb093e9ee1b1debd08a7006ec7533eadc
Zach Mueller <muellerzr@gmail.com>
2023-06-20 11:27:06 -0400

    Try again

--- src/accelerate/test_utils/scripts/test_script.py
+++ src/accelerate/test_utils/scripts/test_script.py
@@ -66,78 +66,80 @@ def print_on(state, process_idx):
 def process_execution_check():
     accelerator = Accelerator()
     num_processes = accelerator.num_processes
+    if accelerator.state.distributed_type == DistributedType.MULTI_GPU:
+        num_gpus_per_node = torch.cuda.device_count()
+    else:
+        num_gpus_per_node = accelerator.state.num_processes
+    # We only run this test on non-multinode
+    if num_gpus_per_node == accelerator.state.num_processes:
+        # Test main_process_first context manager
+        path = Path("check_main_process_first.txt")
+        with accelerator.main_process_first():
+            if accelerator.is_main_process:
+                time.sleep(0.1)  # ensure main process takes longest
+                with open(path, "a+") as f:
+                    f.write("Currently in the main process\n")
+            else:
+                with open(path, "a+") as f:
+                    f.write("Now on another process\n")
+        accelerator.wait_for_everyone()
 
-    # Test main_process_first context manager
-    path = Path("check_main_process_first.txt")
-    with accelerator.main_process_first():
         if accelerator.is_main_process:
-            time.sleep(0.1)  # ensure main process takes longest
-            with open(path, "a+") as f:
-                f.write("Currently in the main process\n")
-        else:
-            with open(path, "a+") as f:
-                f.write("Now on another process\n")
-    accelerator.wait_for_everyone()
+            with open(path, "r") as f:
+                text = "".join(f.readlines())
+            try:
+                assert text.startswith("Currently in the main process\n"), "Main process was not first"
+                if num_processes > 1:
+                    assert text.endswith("Now on another process\n"), "Main process was not first"
+                assert (
+                    text.count("Now on another process\n") == num_gpus_per_node - 1
+                ), f"Only wrote to file {text.count('Now on another process') + 1} times, not {num_gpus_per_node}"
+            except AssertionError:
+                path.unlink()
+                raise
 
-    if accelerator.is_main_process:
-        with open(path, "r") as f:
-            text = "".join(f.readlines())
-        try:
-            if accelerator.state.distributed_type == DistributedType.MULTI_GPU:
-                num_gpus_per_node = torch.cuda.device_count()
-                if num_gpus_per_node == accelerator.state.num_processes:
-                    assert text.startswith("Currently in the main process\n"), "Main process was not first"
-                    if num_processes > 1:
-                        assert text.endswith("Now on another process\n"), "Main process was not first"
-                    assert (
-                        text.count("Now on another process\n") == num_gpus_per_node - 1
-                    ), f"Only wrote to file {text.count('Now on another process') + 1} times, not {num_gpus_per_node}"
-        except AssertionError:
+        if accelerator.is_main_process and path.exists():
             path.unlink()
-            raise
-
-    if accelerator.is_main_process and path.exists():
-        path.unlink()
-    accelerator.wait_for_everyone()
-    # Test the decorators
-    f = io.StringIO()
-    with contextlib.redirect_stdout(f):
-        accelerator.on_main_process(print_main)(accelerator.state)
-    result = f.getvalue().rstrip()
-    if accelerator.is_main_process:
-        assert result == "Printing from the main process 0", f"{result} != Printing from the main process 0"
-    else:
-        assert f.getvalue().rstrip() == "", f'{result} != ""'
-    f.truncate(0)
-    f.seek(0)
-
-    with contextlib.redirect_stdout(f):
-        accelerator.on_local_main_process(print_local_main)(accelerator.state)
-    if accelerator.is_local_main_process:
-        assert f.getvalue().rstrip() == "Printing from the local main process 0"
-    else:
-        assert f.getvalue().rstrip() == ""
-    f.truncate(0)
-    f.seek(0)
-
-    with contextlib.redirect_stdout(f):
-        accelerator.on_last_process(print_last)(accelerator.state)
-    if accelerator.is_last_process:
-        assert f.getvalue().rstrip() == f"Printing from the last process {accelerator.state.num_processes - 1}"
-    else:
-        assert f.getvalue().rstrip() == ""
-    f.truncate(0)
-    f.seek(0)
-
-    for process_idx in range(num_processes):
+        accelerator.wait_for_everyone()
+        # Test the decorators
+        f = io.StringIO()
         with contextlib.redirect_stdout(f):
-            accelerator.on_process(print_on, process_index=process_idx)(accelerator.state, process_idx)
-        if accelerator.process_index == process_idx:
-            assert f.getvalue().rstrip() == f"Printing from process {process_idx}: {accelerator.process_index}"
+            accelerator.on_main_process(print_main)(accelerator.state)
+        result = f.getvalue().rstrip()
+        if accelerator.is_main_process:
+            assert result == "Printing from the main process 0", f"{result} != Printing from the main process 0"
+        else:
+            assert f.getvalue().rstrip() == "", f'{result} != ""'
+        f.truncate(0)
+        f.seek(0)
+
+        with contextlib.redirect_stdout(f):
+            accelerator.on_local_main_process(print_local_main)(accelerator.state)
+        if accelerator.is_local_main_process:
+            assert f.getvalue().rstrip() == "Printing from the local main process 0"
         else:
             assert f.getvalue().rstrip() == ""
         f.truncate(0)
         f.seek(0)
+
+        with contextlib.redirect_stdout(f):
+            accelerator.on_last_process(print_last)(accelerator.state)
+        if accelerator.is_last_process:
+            assert f.getvalue().rstrip() == f"Printing from the last process {accelerator.state.num_processes - 1}"
+        else:
+            assert f.getvalue().rstrip() == ""
+        f.truncate(0)
+        f.seek(0)
+
+        for process_idx in range(num_processes):
+            with contextlib.redirect_stdout(f):
+                accelerator.on_process(print_on, process_index=process_idx)(accelerator.state, process_idx)
+            if accelerator.process_index == process_idx:
+                assert f.getvalue().rstrip() == f"Printing from process {process_idx}: {accelerator.process_index}"
+            else:
+                assert f.getvalue().rstrip() == ""
+            f.truncate(0)
+            f.seek(0)
 
 
 def init_state_check():
