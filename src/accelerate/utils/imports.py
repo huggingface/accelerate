a25c4eacae088f1c863c857d5d6898e5ff9983f6
Zach Mueller <muellerzr@gmail.com>
2023-06-26 09:59:10 -0400

    Swap disable rich (#1640)

--- src/accelerate/utils/imports.py
+++ src/accelerate/utils/imports.py
@@ -161,7 +161,12 @@ def is_boto3_available():
 
 def is_rich_available():
     if _is_package_available("rich"):
-        return not parse_flag_from_env("ACCELERATE_DISABLE_RICH")
+        if "ACCELERATE_DISABLE_RICH" in os.environ:
+            warnings.warn(
+                "`ACCELERATE_DISABLE_RICH` is deprecated and will be removed in v0.22.0 and deactivated by default. Please use `ACCELERATE_ENABLE_RICH` if you wish to use `rich`."
+            )
+            return not parse_flag_from_env("ACCELERATE_DISABLE_RICH", False)
+        return parse_flag_from_env("ACCELERATE_ENABLE_RICH", False)
     return False
 
 
