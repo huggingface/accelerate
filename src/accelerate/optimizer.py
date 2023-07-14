4170f395d16fccb6c0d4fd17a78eca807012484c
YU XINYUAN <xyuaj@connect.ust.hk>
2023-07-14 19:56:45 +0800

    Get rid of calling get_scale() by patching the step method of optimizer. (#1720)
    * Get rid of calling get_scale() by patching the step method of optimizer.
    
    * Fix when step() is already patched by other parties.
    
    * support pickle
    
    * Minor updates.
    
    * Change _accelerate_num_step_called to _accelerate_step_called
    
    ---------
    
    Co-authored-by: YU Xinyuan <yuxinyuan02@corp.netease.com>

--- src/accelerate/optimizer.py
+++ src/accelerate/optimizer.py
@@ -59,7 +59,10 @@ class AcceleratedOptimizer(torch.optim.Optimizer):
         self.gradient_state = GradientState()
         self.device_placement = device_placement
         self._is_overflow = False
-        self._last_scale = None
+
+        self._accelerate_step_called = False
+        self._optimizer_original_step_method = self.optimizer.step
+        self._optimizer_patched_step_method = patch_optimizer_step(self, self.optimizer.step)
 
         # Handle device placement
         if device_placement:
@@ -123,21 +126,20 @@ class AcceleratedOptimizer(torch.optim.Optimizer):
                 optimizer_args = {"closure": closure} if closure is not None else {}
                 xm.optimizer_step(self.optimizer, optimizer_args=optimizer_args)
             elif self.scaler is not None:
-                new_scale = False
-                if self._last_scale is None:
-                    # `get_scale` is an async operation requiring full synchronization
-                    # on CPU and GPUs before finishing. As a result, we store away
-                    # the prior one to reduce the call overhead
-                    self._last_scale = self.scaler.get_scale()
-                    new_scale = True
+                self.optimizer.step = self._optimizer_patched_step_method
+
                 self.scaler.step(self.optimizer, closure)
                 self.scaler.update()
-                scale_after = self.scaler.get_scale()
-                if not new_scale:
-                    # If we reduced the loss scale, it means the optimizer step was skipped because of gradient overflow.
-                    self._is_overflow = scale_after < self._last_scale
-                self._last_scale = scale_after
 
+                if not self._accelerate_step_called:
+                    # If the optimizer step was skipped, gradient overflow was detected.
+                    self._is_overflow = True
+                else:
+                    self._is_overflow = False
+                # Reset the step method to the original one
+                self.optimizer.step = self._optimizer_original_step_method
+                # Reset the indicator
+                self._accelerate_step_called = False
             else:
                 self.optimizer.step(closure)
 
@@ -161,7 +163,23 @@ class AcceleratedOptimizer(torch.optim.Optimizer):
         return self._is_overflow
 
     def __getstate__(self):
-        return self.__dict__.copy()
+        _ignored_keys = [
+            "_accelerate_step_called",
+            "_optimizer_original_step_method",
+            "_optimizer_patched_step_method",
+        ]
+        return {k: v for k, v in self.__dict__.items() if k not in _ignored_keys}
 
     def __setstate__(self, state):
         self.__dict__.update(state)
+        self._accelerate_step_called = False
+        self._optimizer_original_step_method = self.optimizer.step
+        self._optimizer_patched_step_method = patch_optimizer_step(self, self.optimizer.step)
+
+
+def patch_optimizer_step(accelerated_optimizer: AcceleratedOptimizer, method):
+    def patched_step(*args, **kwargs):
+        accelerated_optimizer._accelerate_step_called = True
+        return method(*args, **kwargs)
+
+    return patched_step
--- tests/test_optimizer.py
+++ tests/test_optimizer.py
@@ -19,7 +19,7 @@ import torch
 
 from accelerate import Accelerator
 from accelerate.state import AcceleratorState
-from accelerate.test_utils import require_cpu
+from accelerate.test_utils import require_cpu, require_cuda
 
 
 @require_cpu
@@ -33,4 +33,53 @@ class OptimizerTester(unittest.TestCase):
             pickle.loads(pickle.dumps(optimizer))
         except Exception as e:
             self.fail(f"Accelerated optimizer pickling failed with {e}")
+        AcceleratorState._reset_state()
+
+
+@require_cuda
+class CudaOptimizerTester(unittest.TestCase):
+    def test_accelerated_optimizer_step_was_skipped(self):
+        model = torch.nn.Linear(5, 5)
+        optimizer = torch.optim.SGD(model.parameters(), 0.1)
+        accelerator = Accelerator(mixed_precision="fp16")
+        model, optimizer = accelerator.prepare(model, optimizer)
+
+        loss = model(torch.randn(2, 5, device=accelerator.device)).sum()
+        accelerator.backward(loss)
+        for p in model.parameters():
+            # Fake the gradients, as if there's no overflow
+            p.grad.fill_(0.01)
+
+        optimizer.step()
+        self.assertTrue(optimizer.step_was_skipped is False)
+
+        loss = model(torch.randn(2, 5, device=accelerator.device)).sum()
+        accelerator.backward(loss)
+        for p in model.parameters():
+            p.grad.fill_(0.01)
+        # Manually set the gradients to be NaN, as if there's an overflow
+        p.grad[0] = torch.tensor(float("nan"))
+
+        optimizer.step()
+        self.assertTrue(optimizer.step_was_skipped is True)
+
+        loss = model(torch.randn(2, 5, device=accelerator.device)).sum()
+        accelerator.backward(loss)
+        for p in model.parameters():
+            p.grad.fill_(0.01)
+        # Manually set the gradients to be NaN, as if there's an overflow
+        p.grad[0] = torch.tensor(float("nan"))
+
+        optimizer.step()
+        self.assertTrue(optimizer.step_was_skipped is True)
+
+        loss = model(torch.randn(2, 5, device=accelerator.device)).sum()
+        accelerator.backward(loss)
+        for p in model.parameters():
+            # Fake the gradients, as if there's no overflow
+            p.grad.fill_(0.01)
+
+        optimizer.step()
+        self.assertTrue(optimizer.step_was_skipped is False)
+
         AcceleratorState._reset_state()
