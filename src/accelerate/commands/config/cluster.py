ef5b7787f575c10bd4dbb85dfe203f52b3e24465
Zach Mueller <muellerzr@gmail.com>
2023-07-19 11:00:14 -0400

    Right area

--- src/accelerate/commands/config/cluster.py
+++ src/accelerate/commands/config/cluster.py
@@ -71,10 +71,6 @@ def get_cluster_input():
             int,
             default=1,
         )
-        if (distributed_type == DistributedType.MULTI_GPU) and (num_machines == 1) and (num_processes == 1):
-            raise ValueError(
-                f"Specified distributed type {distributed_type} but only using 1 GPU on a single machine. Please select `No distributed training` for the type of machine you are using."
-            )
 
         if num_machines > 1:
             machine_rank = _ask_options(
@@ -481,6 +477,11 @@ def get_cluster_input():
         )
     else:
         num_processes = 1
+
+    if (distributed_type == DistributedType.MULTI_GPU) and (num_machines == 1) and (num_processes == 1):
+        raise ValueError(
+            f"Specified distributed type {distributed_type} but only using 1 GPU on a single machine. Please select `No distributed training` for the type of machine you are using."
+        )
 
     if (
         distributed_type
