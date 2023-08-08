da9a67ad5d496fe0471302a68c8e703e5f5ecaac
Zach Mueller <muellerzr@gmail.com>
2023-08-08 13:47:48 -0400

    Test

--- src/accelerate/test_utils/scripts/test_notebook.py
+++ src/accelerate/test_utils/scripts/test_notebook.py
@@ -3,5 +3,6 @@ import torch
 
 assert not torch.cuda.is_initialized(), "CUDA was initialized before the test script."
 
+from accelerate import Accelerator  # noqa
 
 assert not torch.cuda.is_initialized(), "CUDA was initialized upon importing the `Accelerator` class."
