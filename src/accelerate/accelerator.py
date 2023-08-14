f67e11afd76f0dad3d4937f503fc1a2eccd5db5a
Daniil Larionov <rexhaif.io@gmail.com>
2023-08-14 17:44:41 +0200

    Fix verify_device_map (#1842)
    * make verify_device_map return True only if device map has more that 1 element
    
    * Fix style and comment
    
    * fix style

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -3059,7 +3059,9 @@ class Accelerator:
         """
         Verifies that `model` has not been prepared with big model inference with a device-map resembling `auto`.
         """
-        # Checks if any of the child module has the attribute `hf_device_map`.
-        has_hf_device_map = any(hasattr(m, "hf_device_map") for m in model.modules())
+        # Checks if any of the child modules has the attribute `hf_device_map` and this map has more than one entry.
+        for m in model.modules():
+            if hasattr(m, "hf_device_map") and len(m.hf_device_map) > 1:
+                return True
 
-        return has_hf_device_map
+        return False
