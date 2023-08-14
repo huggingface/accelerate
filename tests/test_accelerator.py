huggingface/accelerate

commit 2df1a9328ab9449b1cc41786d8d9d0b0ad676884

Solve pickling issues (#872)
* Raise a pickling error if tried to save w/o unwrap

src/accelerate/utils/other.py
@@ -21,7 +21,6 @@ from ..commands.config.default import write_basic_config  # noqa: F401
 from ..state import AcceleratorState
 from .dataclasses import DistributedType
 from .imports import is_deepspeed_available, is_tpu_available
-from .operations import ConvertOutputsToFp32
 
 
 if is_deepspeed_available():
@@ -53,8 +52,13 @@ def extract_model_from_parallel(model, keep_fp32_wrapper: bool = False):
 
     if not keep_fp32_wrapper:
         forward = getattr(model, "forward")
-        if isinstance(forward, ConvertOutputsToFp32):
-            setattr(model, "forward", forward.model_forward)
+        original_forward = model.__dict__.pop("_original_forward", None)
+        if original_forward is not None:
+            while hasattr(forward, "__wrapped__"):
+                forward = forward.__wrapped__
+                if forward == original_forward:
+                    break
+            model.forward = forward
     return model
 
 

src/accelerate/accelerator.py
@@ -926,6 +926,7 @@ class Accelerator:
             kwargs = self.ddp_handler.to_kwargs() if self.ddp_handler is not None else {}
             model = torch.nn.parallel.DistributedDataParallel(model, **kwargs)
         if self.native_amp:
+            model._original_forward = model.forward
             if self.mixed_precision == "fp16" and is_torch_version(">=", "1.10"):
                 model.forward = torch.cuda.amp.autocast(dtype=torch.float16)(model.forward)
             elif self.mixed_precision == "bf16" and self.distributed_type != DistributedType.TPU:

src/accelerate/utils/operations.py
@@ -17,6 +17,7 @@ A set of basic tensor ops compatible with tpu, gpu, and multigpu
 """
 
 
+import pickle
 from functools import update_wrapper
 from typing import Any, Mapping
 
@@ -473,8 +474,6 @@ class ConvertOutputsToFp32:
     Decorator to apply to a function outputing tensors (like a model forward pass) that ensures the outputs in FP16
     precision will be convert back to FP32.
 
-    Use a class instead of a decorator because otherwise, the prepared model can no longer be pickled (issue #273).
-
     Args:
         model_forward (`Callable`):
             The function which outputs we want to treat.
@@ -489,6 +488,11 @@ class ConvertOutputsToFp32:
 
     def __call__(self, *args, **kwargs):
         return convert_to_fp32(self.model_forward(*args, **kwargs))
+
+    def __getstate__(self):
+        raise pickle.PicklingError(
+            "Cannot pickle a prepared model with automatic mixed precision, please unwrap the model with `Accelerator.unwrap_model(model)` before pickling it."
+        )
 
 
 convert_outputs_to_fp32 = ConvertOutputsToFp32

tests/test_utils.py
@@ -19,8 +19,15 @@ from collections import UserDict, namedtuple
 
 import torch
 
+from accelerate.test_utils.testing import require_cuda
 from accelerate.test_utils.training import RegressionModel
-from accelerate.utils import convert_outputs_to_fp32, find_device, patch_environment, send_to_device
+from accelerate.utils import (
+    convert_outputs_to_fp32,
+    extract_model_from_parallel,
+    find_device,
+    patch_environment,
+    send_to_device,
+)
 
 
 ExampleNamedTuple = namedtuple("ExampleNamedTuple", "a b c")
@@ -74,9 +81,20 @@ class UtilsTester(unittest.TestCase):
         self.assertNotIn("AA", os.environ)
         self.assertNotIn("BB", os.environ)
 
-    def test_convert_to_32_lets_model_pickle(self):
+    def test_can_undo_convert_outputs(self):
         model = RegressionModel()
+        model._original_forward = model.forward
         model.forward = convert_outputs_to_fp32(model.forward)
+        model = extract_model_from_parallel(model)
+        _ = pickle.dumps(model)
+
+    @require_cuda
+    def test_can_undo_fp16_conversion(self):
+        model = RegressionModel()
+        model._original_forward = model.forward
+        model.forward = torch.cuda.amp.autocast(dtype=torch.float16)(model.forward)
+        model.forward = convert_outputs_to_fp32(model.forward)
+        model = extract_model_from_parallel(model)
         _ = pickle.dumps(model)
 
     def test_find_device(self):

commit 645805855908aa000653725e162a255fba4969ce

FIX: Bug with unwrap_model and keep_fp32_wrapper=False (#1838)
Using accelerator.unwrap_model(model, keep_fp32_wrapper=False) results
in a defective forward method. This bug was (probably) introduced in
PR #872.

Wrapping the method in MethodType (as elsewhere in code) resolves the
issue.

src/accelerate/utils/other.py
@@ -15,6 +15,7 @@
 import os
 import socket
 from contextlib import contextmanager
+from types import MethodType
 
 import torch
 
@@ -82,7 +83,7 @@ def extract_model_from_parallel(model, keep_fp32_wrapper: bool = True):
                 forward = forward.__wrapped__
                 if forward == original_forward:
                     break
-            model.forward = forward
+            model.forward = MethodType(forward, model)
         if getattr(model, "_converted_to_transformer_engine", False):
             convert_model(model, to_transformer_engine=False)
 

tests/test_accelerator.py
@@ -1,6 +1,7 @@
 import inspect
 import json
 import os
+import pickle
 import tempfile
 from unittest.mock import patch
 
@@ -339,3 +340,35 @@ class AcceleratorTester(AccelerateTestCase):
         cmd = ["accelerate", "launch", script]
         with patch_environment(omp_num_threads=1):
             execute_subprocess_async(cmd, env=os.environ.copy())
+
+    @require_cuda
+    def test_can_unwrap_model_fp16(self):
+        # test for a regression introduced in #872
+        # before the fix, after unwrapping with keep_fp32_wrapper=False, there would be the following error:
+        # Linear.forward() missing 1 required positional argument: 'input'
+        model = create_components()[0]
+        accelerator = Accelerator(mixed_precision="fp16")
+        inputs = torch.randn(10, 2).cuda()
+        model = accelerator.prepare(model)
+        model(inputs)  # sanity check that this works
+
+        model = accelerator.unwrap_model(model, keep_fp32_wrapper=False)
+        model(inputs)  # check that this still works
+
+        # check that pickle roundtrip works
+        model_loaded = pickle.loads(pickle.dumps(model))
+        model_loaded(inputs)
+
+    def test_can_unwrap_model(self):
+        model = create_components()[0]
+        accelerator = Accelerator(mixed_precision="no", cpu=True)
+        inputs = torch.randn(10, 2)
+        model = accelerator.prepare(model)
+        model(inputs)  # sanity check that this works
+
+        model = accelerator.unwrap_model(model, keep_fp32_wrapper=False)
+        model(inputs)  # check that this still works
+
+        # check that pickle roundtrip works
+        model_loaded = pickle.loads(pickle.dumps(model))
+        model_loaded(inputs)