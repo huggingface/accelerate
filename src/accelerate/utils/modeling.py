3cf7dee5760f1f3d10490ad003a1c932c799ceae
Marc Sun <57196510+SunMarc@users.noreply.github.com>
2023-08-17 10:46:49 -0400

    Loading logic safetensors (#1853)
    * add logic in loading for safetensors
    
    * fix style

--- src/accelerate/utils/modeling.py
+++ src/accelerate/utils/modeling.py
@@ -28,7 +28,7 @@ import torch
 import torch.nn as nn
 
 from ..state import AcceleratorState
-from .constants import WEIGHTS_NAME
+from .constants import SAFE_WEIGHTS_NAME, WEIGHTS_NAME
 from .dataclasses import AutocastKwargs, CustomDtype, DistributedType
 from .imports import is_mps_available, is_npu_available, is_safetensors_available, is_xpu_available
 from .offload import load_offloaded_weight, offload_weight, save_offload_index
@@ -1236,7 +1236,7 @@ def load_checkpoint_in_model(
             - a path to a file containing a whole model state dict
             - a path to a `.json` file containing the index to a sharded checkpoint
             - a path to a folder containing a unique `.index.json` file and the shards of a checkpoint.
-            - a path to a folder containing a unique pytorch_model.bin file.
+            - a path to a folder containing a unique pytorch_model.bin or a model.safetensors file.
         device_map (`Dict[str, Union[int, str, torch.device]]`, *optional*):
             A map that specifies where each submodule should go. It doesn't need to be refined to each parameter/buffer
             name, once a given module name is inside, every submodule of it will be sent to the same device.
@@ -1288,15 +1288,18 @@ def load_checkpoint_in_model(
             checkpoint_files = [checkpoint]
     elif os.path.isdir(checkpoint):
         # check if the whole state dict is present
-        potential_state = [f for f in os.listdir(checkpoint) if f == WEIGHTS_NAME]
-        if len(potential_state) == 1:
-            checkpoint_files = [os.path.join(checkpoint, potential_state[0])]
+        potential_state_bin = [f for f in os.listdir(checkpoint) if f == WEIGHTS_NAME]
+        potential_state_safetensor = [f for f in os.listdir(checkpoint) if f == SAFE_WEIGHTS_NAME]
+        if len(potential_state_bin) == 1:
+            checkpoint_files = [os.path.join(checkpoint, potential_state_bin[0])]
+        elif len(potential_state_safetensor) == 1:
+            checkpoint_files = [os.path.join(checkpoint, potential_state_safetensor[0])]
         else:
             # otherwise check for sharded checkpoints
             potential_index = [f for f in os.listdir(checkpoint) if f.endswith(".index.json")]
             if len(potential_index) == 0:
                 raise ValueError(
-                    f"{checkpoint} is not a folder containing a `.index.json` file or a {WEIGHTS_NAME} file"
+                    f"{checkpoint} is not a folder containing a `.index.json` file or a {WEIGHTS_NAME} or a {SAFE_WEIGHTS_NAME} file"
                 )
             elif len(potential_index) == 1:
                 index_filename = os.path.join(checkpoint, potential_index[0])
