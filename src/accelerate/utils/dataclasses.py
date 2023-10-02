51349144d87c0ab8f98ed13f42398f7b43bb507c
Abhilash Majumder <30946547+abhilash1910@users.noreply.github.com>
2023-10-03 00:20:32 +0530

    Update src/accelerate/utils/dataclasses.py
    Co-authored-by: Zach Mueller <muellerzr@gmail.com>

--- src/accelerate/utils/dataclasses.py
+++ src/accelerate/utils/dataclasses.py
@@ -917,10 +917,8 @@ class FullyShardedDataParallelPlugin:
         self.activation_checkpointing = str_to_bool(os.environ.get(prefix + "ACTIVATION_CHECKPOINTING", "False")) == 1
 
         if self.sync_module_states:
-            if is_xpu_available():
-                self.param_init_fn = lambda x: x.to_empty(device=torch.xpu.current_device(), recurse=False)
-            else:
-                self.param_init_fn = lambda x: x.to_empty(device=torch.cuda.current_device(), recurse=False)
+            device = torch.cuda.current_device() if not is_xpu_available() else torch.xpu.current_device()
+            self.param_init_fn = lambda x: x.to_empty(device=device, recurse=False)
 
     @staticmethod
     def get_module_class_from_name(module, name):
