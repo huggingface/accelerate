0532b49d24d862b7e68a0bd404cec391ffd08127
Benjamin Bossan <benjamin.bossan@gmail.com>
2023-10-09 14:31:34 +0200

    Dummy change to actually trigger full CI
    Empty commits don't trigger whole CI, so add a dummy change.

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# FOOBAR
 from setuptools import setup
 from setuptools import find_packages
 
