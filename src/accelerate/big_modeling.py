c3ec7ff5a9e1e3bddd3be0061e6362ff1bcb08a1
Marc Sun <57196510+SunMarc@users.noreply.github.com>
2023-10-24 22:05:23 +0200

    Add logs offloading (#2075)
    * add logs
    
    * fix comm
    
    * rework comment

--- src/accelerate/big_modeling.py
+++ src/accelerate/big_modeling.py
@@ -402,8 +402,22 @@ def dispatch_model(
             skip_keys=skip_keys,
             preload_module_classes=preload_module_classes,
         )
+
+        # warn if there is any params on the meta device
+        if len(offload) > 0:
+            offloaded_devices_str = " and ".join(
+                [device for device in set(device_map.values()) if device in ("cpu", "disk")]
+            )
+            logging.warning(
+                f"Some parameters are on the meta device device because they were offloaded to the {offloaded_devices_str}."
+            )
+
         # Attaching the hook may break tied weights, so we retie them
         retie_parameters(model, tied_params)
+
+        # warn if there is any params on the meta device
+        if len(offload) > 0:
+            logging.warn("Some parameters are on the meta device device because they were offloaded to the cpu/disk.")
 
         # add warning to cuda and to method
         def add_warning(fn, model):
