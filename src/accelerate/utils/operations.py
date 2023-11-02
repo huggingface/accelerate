f3f8572bccbd4d64d36c566ee1eef838d0abc1c9
YuryYakhno <52328128+YuryYakhno@users.noreply.github.com>
2023-11-02 19:15:25 +0300

    Move error check to cover all empty iterables

--- src/accelerate/utils/operations.py
+++ src/accelerate/utils/operations.py
@@ -231,11 +231,12 @@ def find_batch_size(data):
     Returns:
         `int`: The batch size.
     """
+    if isinstance(data, (tuple, list, Mapping)) and (len(data) == 0):
+        raise ValueError(f"Cannot find the batch size from empty {type(data)}.")
+
     if isinstance(data, (tuple, list)):
         return find_batch_size(data[0])
     elif isinstance(data, Mapping):
-        if len(data) == 0:
-            raise ValueError(f"Can't find the batch size from empty {type(data)}.")
         for k in data.keys():
             return find_batch_size(data[k])
     elif not isinstance(data, torch.Tensor):
