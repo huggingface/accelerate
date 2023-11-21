1658d30140931f014bcce78f26750f663dbaa1f0
Benjamin Badger <54602201+blbadger@users.noreply.github.com>
2023-11-21 11:20:42 -0500

    Update src/accelerate/utils/modeling.py
    Co-authored-by: Marc Sun <57196510+SunMarc@users.noreply.github.com>

--- src/accelerate/utils/modeling.py
+++ src/accelerate/utils/modeling.py
@@ -1267,7 +1267,7 @@ def get_state_dict_offloaded_model(model: nn.Module):
 
         for key in module_state_dict:
             # ignore placeholder parameters that are still on the meta device
-            if str(module_state_dict[key].device) == "meta":
+            if module_state_dict[key].device == torch.device("meta"):
                 placeholders.append(name + f".{key}")
                 continue
             params = module_state_dict[key]
