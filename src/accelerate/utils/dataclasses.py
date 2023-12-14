f564135f414a655349c64af4acb258706c549d8c
Florian Seiler <fs@jerocom.de>
2023-12-14 11:59:34 +0000

    fixing auto_wrap_policy for PEFT with FSDP and fixing style

--- src/accelerate/utils/dataclasses.py
+++ src/accelerate/utils/dataclasses.py
@@ -993,11 +993,16 @@ class FullyShardedDataParallelPlugin:
                     transformer_layer_cls=transformer_cls_to_wrap,
                 )
 
-                # In an FSDP setting PEFT models require individually wrapping unfrozen parameters 
-                
+                # In an FSDP setting PEFT models require individually wrapping unfrozen parameters
+
                 if isinstance(model, PeftModel):
                     print("PEFT wrapping")
-                    from torch.distributed.fsdp.wrap import _or_policy, lambda_auto_wrap_policy, transformer_auto_wrap_policy
+                    from torch.distributed.fsdp.wrap import (
+                        _or_policy,
+                        lambda_auto_wrap_policy,
+                        transformer_auto_wrap_policy,
+                    )
+
                     def lambda_policy_fn(module):
                         if (
                             len(list(module.named_children())) == 0
@@ -1009,9 +1014,9 @@ class FullyShardedDataParallelPlugin:
 
                     lambda_policy = functools.partial(lambda_auto_wrap_policy, lambda_fn=lambda_policy_fn)
                     auto_wrap_policy = functools.partial(_or_policy, policies=[lambda_policy, auto_wrap_policy])
-                    
+
                 self.auto_wrap_policy = auto_wrap_policy
-                
+
             elif auto_wrap_policy == FSDP_AUTO_WRAP_POLICY[1]:
                 min_num_params = int(os.environ.get("FSDP_MIN_NUM_PARAMS", 0))
                 if min_num_params > 0:
