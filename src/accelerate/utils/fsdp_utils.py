bc1bcebbac61b294a21942da7541993b08431d41
Ajay Patel <patel.ajay285@gmail.com>
2024-01-10 08:44:43 -0500

    Fix quality

--- src/accelerate/utils/fsdp_utils.py
+++ src/accelerate/utils/fsdp_utils.py
@@ -17,7 +17,7 @@ import torch
 
 from ..logging import get_logger
 from .constants import FSDP_MODEL_NAME, FSDP_PYTORCH_VERSION, OPTIMIZER_NAME
-from .imports import is_torch_distributed_available, is_peft_available
+from .imports import is_peft_available, is_torch_distributed_available
 from .other import extract_model_from_parallel
 from .versions import is_torch_version
 
