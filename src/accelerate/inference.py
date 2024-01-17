e3f6b99b68d710d90a6c6b17c94718463eb42ff5
[[ -z $EMAIL ]] && read -e -p "Enter your email (for git configuration): " EMAIL <muellerzr@gmail.com>
2024-01-17 11:20:37 -0500

    Put split_points in pipelien

--- src/accelerate/inference.py
+++ src/accelerate/inference.py
@@ -42,16 +42,13 @@ def generate_device_map(model, num_processes: int = 1, no_split_module_classes=N
     return device_map
 
 
-def build_pipeline(model, device_map, args, kwargs) -> PipelineStage:
+def build_pipeline(model, split_points, args, kwargs) -> PipelineStage:
     """
     Attaches the split points to the model based on `self.device_map` and generates a `PipelineStage`. Requires passing
     in needed `args` and `kwargs` as the model needs on the CPU.
     """
     # We need to annotate the split points in the model for PiPPy
     state = PartialState()
-    split_points = []
-    for i in range(1, state.num_processes):
-        split_points.append(next(k for k, v in device_map.items() if v == i))
     annotate_split_points(model, {split_point: PipeSplitWrapper.SplitPoint.BEGINNING for split_point in split_points})
     pipe = Pipe.from_tracing(model, num_chunks=state.num_processes, example_args=args, example_kwargs=kwargs)
     stage = PipelineStage(pipe, state.local_process_index, device=state.device)
@@ -71,17 +68,19 @@ def pippy_forward(forward, *args, **kwargs):
     return output
 
 
-def prepare_pippy(model, device_map="auto", no_split_module_classes=[], example_args=(), example_kwargs={}):
+def prepare_pippy(model, split_points="auto", no_split_module_classes=[], example_args=(), example_kwargs={}):
     """
     Wraps `model` for PipelineParallelism
     """
+    state = PartialState()
     example_args = send_to_device(example_args, "cpu")
     example_kwargs = send_to_device(example_kwargs, "cpu")
-    if device_map == "auto":
-        device_map = generate_device_map(
-            model, PartialState().num_processes, no_split_module_classes=no_split_module_classes
-        )
-    stage = build_pipeline(model, device_map, example_args, example_kwargs)
+    if split_points == "auto":
+        device_map = generate_device_map(model, state.num_processes, no_split_module_classes=no_split_module_classes)
+        split_points = []
+        for i in range(1, state.num_processes):
+            split_points.append(next(k for k, v in device_map.items() if v == i))
+    stage = build_pipeline(model, split_points, example_args, example_kwargs)
     model._original_forward = model.forward
     model._original_call = model.__call__
     model.pippy_stage = stage
