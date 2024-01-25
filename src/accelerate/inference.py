e3cb2888b3387071c58612f9c4ff30b160a140e3
[[ -z $EMAIL ]] && read -e -p "Enter your email (for git configuration): " EMAIL <muellerzr@gmail.com>
2024-01-25 09:07:00 -0500

    Allow for users to pass in max_meory

--- src/accelerate/inference.py
+++ src/accelerate/inference.py
@@ -15,25 +15,27 @@ from .utils import (
 )
 
 
-def generate_device_map(model, num_processes: int = 1, no_split_module_classes=None):
+def generate_device_map(model, num_processes: int = 1, no_split_module_classes=None, max_memory: dict = None):
     """
     Calculates the device map for `model` with an offset for PiPPy
     """
     if num_processes == 1:
         return infer_auto_device_map(model, no_split_module_classes=no_split_module_classes, clean_result=False)
-    model_size, shared = calculate_maximum_sizes(model)
+    if max_memory is None:
+        model_size, shared = calculate_maximum_sizes(model)
 
-    # Split into `n` chunks for each GPU
-    memory = (model_size + shared[0]) / num_processes
-    memory = convert_bytes(memory)
-    value, ending = memory.split(" ")
+        # Split into `n` chunks for each GPU
+        memory = (model_size + shared[0]) / num_processes
+        memory = convert_bytes(memory)
+        value, ending = memory.split(" ")
 
-    # Add a chunk to deal with potential extra shared memory instances
-    memory = math.ceil(float(value)) * 1.1
-    memory = f"{memory} {ending}"
+        # Add a chunk to deal with potential extra shared memory instances
+        memory = math.ceil(float(value)) * 1.1
+        memory = f"{memory} {ending}"
+        max_memory = {i: memory for i in range(num_processes)}
     device_map = infer_auto_device_map(
         model,
-        max_memory={i: memory for i in range(num_processes)},
+        max_memory=max_memory,
         no_split_module_classes=no_split_module_classes,
         clean_result=False,
     )
