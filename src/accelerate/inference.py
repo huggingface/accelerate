06b138d84537ffb2d1d404f2f198a0446e8d7ec3
[[ -z $EMAIL ]] && read -e -p "Enter your email (for git configuration): " EMAIL <muellerzr@gmail.com>
2024-02-06 13:10:43 -0500

    Try again

--- src/accelerate/inference.py
+++ src/accelerate/inference.py
@@ -48,14 +48,16 @@ def generate_device_map(model, num_processes: int = 1, no_split_module_classes=N
 
 def find_pippy_batch_size(args, kwargs):
     found_batch_size = None
-    for arg in args:
-        found_batch_size = ignorant_find_batch_size(arg)
-        if found_batch_size is not None:
-            break
-    for kwarg in kwargs.values():
-        found_batch_size = ignorant_find_batch_size(kwarg)
-        if found_batch_size is not None:
-            break
+    if args is not None:
+        for arg in args:
+            found_batch_size = ignorant_find_batch_size(arg)
+            if found_batch_size is not None:
+                break
+    if kwargs is not None and found_batch_size is None:
+        for kwarg in kwargs.values():
+            found_batch_size = ignorant_find_batch_size(kwarg)
+            if found_batch_size is not None:
+                break
     return found_batch_size
 
 
