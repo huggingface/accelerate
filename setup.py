5eb7ab9f6afb0fed38cb54741e243180d6f8bc5d
Zach Mueller <muellerzr@gmail.com>
2024-02-13 10:14:49 -0500

    Adds tag v0.27.2 for pypi

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ extras["sagemaker"] = [
 
 setup(
     name="accelerate",
-    version="0.27.1",
+    version="0.27.2",
     description="Accelerate",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
--- src/accelerate/__init__.py
+++ src/accelerate/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.27.1"
+__version__ = "0.27.2"
 
 from .accelerator import Accelerator
 from .big_modeling import (
