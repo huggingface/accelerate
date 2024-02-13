bc7e411ab6076b65cc8d7f35dea92f9d05e8456a
Marc Sun <marc@huggingface.co>
2024-02-13 21:41:51 +0100

    oups

--- src/accelerate/big_modeling.py
+++ src/accelerate/big_modeling.py
@@ -441,9 +441,13 @@ def dispatch_model(
         def add_warning(fn, model):
             @wraps(fn)
             def wrapper(*args, **kwargs):
-                to_device = torch._C._nn._parse_to(*args, **kwargs)[0]
-                if to_device is not None:
-                    logger.warning("You shouldn't move a model that is dispatched using accelerate hooks.")
+                warning_msg = "You shouldn't move a model that is dispatched using accelerate hooks."
+                if str(fn.__name__) == "to":
+                    to_device = torch._C._nn._parse_to(*args, **kwargs)[0]
+                    if to_device is not None:
+                        logger.warning(warning_msg)
+                else:
+                    logger.warning(warning_msg)
                 for param in model.parameters():
                     if param.device == torch.device("meta"):
                         raise RuntimeError("You can't move a model that has some modules offloaded to cpu or disk.")
