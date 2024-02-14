b1b16faa5d94abb6fdd44ffcc1f56a76e5591f1f
Benjamin Bossan <benjamin.bossan@gmail.com>
2024-02-14 12:38:17 +0100

    Fix syntax issues
    Not sure how to add a link of the type is `list[SomeType]`, so just
    removed it for now.

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -225,8 +225,8 @@ class Accelerator:
         step_scheduler_with_optimizer (`bool`, *optional`, defaults to `True`):
             Set `True` if the learning rate scheduler is stepped at the same time as the optimizer, `False` if only
             done under certain circumstances (at the end of each epoch, for instance).
-        kwargs_handlers (`list[~utils.KwargsHandler]`, *optional*)
-            A list of `[~utils.KwargsHandler]` to customize how the objects related to distributed training or mixed
+        kwargs_handlers (`list[KwargsHandler]`, *optional*)
+            A list of [`~utils.KwargsHandler`] to customize how the objects related to distributed training or mixed
             precision are created. See [kwargs](kwargs) for more information.
         dynamo_backend (`str` or [`~utils.DynamoBackend`], *optional*, defaults to `"no"`):
             Set to one of the possible dynamo backends to optimize your training with torch dynamo.
