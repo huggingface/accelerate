f028fb5cbcc694bca3e8fcd7d056b52547e53a08
Zach Mueller <muellerzr@gmail.com>
2024-02-28 11:39:55 -0500

    Tense

--- src/accelerate/utils/environment.py
+++ src/accelerate/utils/environment.py
@@ -59,7 +59,7 @@ def parse_choice_from_env(key, default="no"):
 
 def are_libraries_initialized(*library_names: str) -> List[str]:
     """
-    Checks if any of `library_names` are imported in the environment. Will return any names that were.
+    Checks if any of `library_names` are imported in the environment. Will return any names that are.
     """
     return [lib_name for lib_name in library_names if lib_name in sys.modules.keys()]
 
