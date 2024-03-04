6cf8221a09173236b2a02cc6a760b4179b4c6f0e
Luis Capelo <953118+luiscape@users.noreply.github.com>
2024-03-04 14:18:17 -0500

    Don't manage `PYTORCH_NVML_BASED_CUDA_CHECK` when calling `accelerate.utils.imports.is_cuda_available()` (#2524)
    * Don't manage PYTORCH_NVML_BASED_CUDA_CHECK
    
    PYTORCH_NVML_BASED_CUDA_CHECK will use an NVML-based check when
    determining how many devices are available. That's useful for preventing
    CUDA initialization when doing that check (or calling
    `torch.cuda.is_available()`). Instead of manipulating that env var, one
    can call the torch utility `_device_count_nvml` directly preventing the
    manipulation of the env var.
    
    * Uses env var instead of private torch function
    
    * Fixes flake8 check

--- src/accelerate/utils/imports.py
+++ src/accelerate/utils/imports.py
@@ -98,11 +98,16 @@ def is_cuda_available():
     Checks if `cuda` is available via an `nvml-based` check which won't trigger the drivers and leave cuda
     uninitialized.
     """
+    pytorch_nvml_based_cuda_check_previous_value = os.environ.get("PYTORCH_NVML_BASED_CUDA_CHECK")
     try:
         os.environ["PYTORCH_NVML_BASED_CUDA_CHECK"] = str(1)
         available = torch.cuda.is_available()
     finally:
-        os.environ.pop("PYTORCH_NVML_BASED_CUDA_CHECK", None)
+        if pytorch_nvml_based_cuda_check_previous_value:
+            os.environ["PYTORCH_NVML_BASED_CUDA_CHECK"] = pytorch_nvml_based_cuda_check_previous_value
+        else:
+            os.environ.pop("PYTORCH_NVML_BASED_CUDA_CHECK", None)
+
     return available
 
 
