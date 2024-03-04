4dab5c741257407328996cd4d2ac0b6e2f99f4f3
Luis Capelo <luis@modal.com>
2024-03-04 18:50:41 +0000

    Uses env var instead of private torch function

--- src/accelerate/utils/imports.py
+++ src/accelerate/utils/imports.py
@@ -96,10 +96,19 @@ def is_fp8_available():
 def is_cuda_available():
     """
     Checks if `cuda` is available via an `nvml-based` check which won't trigger the drivers and leave cuda
-    uninitialized. This is similar to setting the environment variable PYTORCH_NVML_BASED_CUDA_CHECK=1.
-    See: https://pytorch.org/docs/stable/notes/cuda.html#device-agnostic-code
+    uninitialized.
     """
-    return torch.cuda._device_count_nvml() > 0
+    pytorch_nvml_based_cuda_check_previous_value = os.getenv("PYTORCH_NVML_BASED_CUDA_CHECK")
+    try:
+        os.environ["PYTORCH_NVML_BASED_CUDA_CHECK"] = str(1)
+        available = torch.cuda.is_available()
+    finally:
+        if pytorch_nvml_based_cuda_check_previous_value:
+            os.environ["PYTORCH_NVML_BASED_CUDA_CHECK"] = pytorch_nvml_based_cuda_check_previous_value
+        else:
+            os.environ.pop("PYTORCH_NVML_BASED_CUDA_CHECK", None)
+
+    return available
 
 
 @lru_cache
