c12cb6fb75a769e7a616b47d6790b7a3c0a0bfdc
iefgnoix <isaacwxf23@gmail.com>
2024-03-11 22:34:47 +0000

    not setting the dataloader.batch_sampler

--- src/accelerate/data_loader.py
+++ src/accelerate/data_loader.py
@@ -887,7 +887,6 @@ def prepare_data_loader(
         generator = torch.Generator().manual_seed(42)
         dataloader.generator = generator
         dataloader.sampler.generator = generator
-        dataloader.batch_sampler.sampler = dataloader.sampler
     # No change if no multiprocess
     if (num_processes != 1 or state.distributed_type == DistributedType.MEGATRON_LM) and not dispatch_batches:
         if isinstance(new_dataset, IterableDataset):
