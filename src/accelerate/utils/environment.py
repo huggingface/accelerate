ce20684e7add9cfe01db28b28af5b2c9bc6f2fa9
Zach Mueller <muellerzr@gmail.com>
2024-03-15 02:26:51 -0400

    Include working driver

--- src/accelerate/utils/environment.py
+++ src/accelerate/utils/environment.py
@@ -18,6 +18,7 @@ import subprocess
 import sys
 from shutil import which
 from typing import List
+from packaging.version import parse
 
 import torch
 
@@ -63,12 +64,9 @@ def are_libraries_initialized(*library_names: str) -> List[str]:
     """
     return [lib_name for lib_name in library_names if lib_name in sys.modules.keys()]
 
-
-def get_gpu_info():
+def _nvidia_smi():
     """
-    Gets GPU count and names using `nvidia-smi` instead of torch to not initialize CUDA.
-
-    Largely based on the `gputil` library.
+    Calls nvidia-smi
     """
     if platform.system() == "Windows":
         # If platform is Windows and nvidia-smi can't be found in path
@@ -78,9 +76,17 @@ def get_gpu_info():
             command = "%s\\Program Files\\NVIDIA Corporation\\NVSMI\\nvidia-smi.exe" % os.environ["systemdrive"]
     else:
         command = "nvidia-smi"
+    return command
+    
+def get_gpu_info():
+    """
+    Gets GPU count and names using `nvidia-smi` instead of torch to not initialize CUDA.
+
+    Largely based on the `gputil` library.
+    """
     # Returns as list of `n` GPUs and their names
     output = subprocess.check_output(
-        [command, "--query-gpu=count,name", "--format=csv,noheader"], universal_newlines=True
+        [_nvidia_smi(), "--query-gpu=count,name", "--format=csv,noheader"], universal_newlines=True
     )
     output = output.strip()
     gpus = output.split(os.linesep)
@@ -88,6 +94,18 @@ def get_gpu_info():
     gpu_count = len(gpus)
     gpu_names = [gpu.split(",")[1].strip() for gpu in gpus]
     return gpu_names, gpu_count
+
+def get_driver_version():
+    """
+    Returns the driver version
+
+    In the case of multiple GPUs, will return the first.
+    """
+    output = subprocess.check_output(
+        [_nvidia_smi(), "--query-gpu=driver_version", "--format=csv,noheader"], universal_newlines=True
+    )
+    output = output.strip()
+    return output.split(os.linesep)[0]
 
 
 def check_cuda_p2p_ib_support():
@@ -107,7 +125,12 @@ def check_cuda_p2p_ib_support():
                 for device_name in device_names
                 for unsupported_device in unsupported_devices
             ):
-                return False
+                # Check if they have the right driver version
+                acceptable_driver_version = "550.40.07"
+                current_driver_version = get_driver_version()
+                if parse(current_driver_version) < parse(acceptable_driver_version):
+                    return False
+                return True
     except Exception:
         pass
     return True
