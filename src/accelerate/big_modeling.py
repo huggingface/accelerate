18121523920168d79337fa320aa4cf0c8c82ac57
Marc Sun <57196510+SunMarc@users.noreply.github.com>
2024-03-20 12:30:41 -0400

    Add log message for RTX 4000 series when performing multi-gpu inference with device_map (#2557)
    * add log message for RTX 4000 series when using device_map multi-gpu
    
    * style
    
    * style
    
    * switch to warning
    
    * Update src/accelerate/big_modeling.py
    
    Co-authored-by: Zach Mueller <muellerzr@gmail.com>
    
    ---------
    
    Co-authored-by: Zach Mueller <muellerzr@gmail.com>

--- src/accelerate/big_modeling.py
+++ src/accelerate/big_modeling.py
@@ -31,6 +31,7 @@ from .hooks import (
 )
 from .utils import (
     OffloadedWeightsLoader,
+    check_cuda_p2p_ib_support,
     check_device_map,
     extract_submodules_state_dict,
     find_tied_parameters,
@@ -466,6 +467,14 @@ def dispatch_model(
         else:
             model.cuda = add_warning(model.cuda, model)
 
+        # Check if we are using multi-gpus with RTX 4000 series
+        use_multi_gpu = len([device for device in set(device_map.values()) if device not in ("cpu", "disk")]) > 1
+        if use_multi_gpu and not check_cuda_p2p_ib_support():
+            logger.warning(
+                "We've detected an older driver with an RTX 4000 series GPU. These drivers have issues with P2P. "
+                "This can affect the multi-gpu inference when using accelerate device_map."
+                "Please make sure to update your driver to the latest version which resolves this."
+            )
     else:
         device = list(device_map.values())[0]
         # `torch.Tensor.to(<int num>)` is not supported by `torch_npu` (see this [issue](https://github.com/Ascend/pytorch/issues/16)).
