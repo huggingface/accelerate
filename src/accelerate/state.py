df485ae1e327b2abea460c12743aecd0c156337c
Zach Mueller <muellerzr@gmail.com>
2024-04-09 06:33:38 -0400

    Parenthesis on xpu_available (#2639)

--- src/accelerate/state.py
+++ src/accelerate/state.py
@@ -193,7 +193,7 @@ class PartialState:
                             )
                         from deepspeed import comm as dist
 
-                        if is_xpu_available and is_ccl_available():
+                        if is_xpu_available() and is_ccl_available():
                             os.environ["CCL_PROCESS_LAUNCHER"] = "none"
                             os.environ["CCL_LOCAL_SIZE"] = os.environ.get("LOCAL_WORLD_SIZE", "1")
                             os.environ["CCL_LOCAL_RANK"] = os.environ.get("LOCAL_RANK", "0")
