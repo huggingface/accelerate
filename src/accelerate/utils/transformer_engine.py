782f986032b8ae1458f6155522fb6307ffd499d2
[[ -z $EMAIL ]] && read -e -p "Enter your email (for git configuration): " EMAIL <muellerzr@gmail.com>
2024-04-11 16:12:16 -0400

    Revert + maintain

--- src/accelerate/utils/transformer_engine.py
+++ src/accelerate/utils/transformer_engine.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import torch
 import torch.nn as nn
 
 from .imports import is_fp8_available
