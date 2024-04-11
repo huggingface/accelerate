2c84b4627666068960691143f35fa4a46181846d
Richard Brown <richard.brown@synthesia.io>
2024-04-11 14:40:36 +0200

    add unit test

---
+++ tests/test_tqdm.py
@@ -0,0 +1,28 @@
+# Copyright 2022 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from accelerate.utils.tqdm import tqdm
+
+class TqdmTester(unittest.TestCase):
+    def test_tqdm(self):
+        expected = [0, 1, 2]
+        assert list(tqdm(range(3), main_process_only=True)) == expected
+        assert list(tqdm(main_process_only=True, iterable=range(3))) == expected
+
+        assert list(tqdm(range(3), main_process_only=False)) == expected
+        assert list(tqdm(main_process_only=False, iterable=range(3))) == expected
+
+        assert list(tqdm(range(3))) == expected
