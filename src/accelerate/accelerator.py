c650673e2922d12f158cef33c25c35206b167550
Yu Chin Fabian Lim <flim@sg.ibm.com>
2024-04-16 01:44:35 +0000

    improvements to float32 casting

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -1448,20 +1448,58 @@ class Accelerator:
                             auto_wrap_policy=fsdp_plugin.auto_wrap_policy,
                         )
 
-                # NOTE: do we also need to check trainer.args.bf16 and trainer.args.fp16?
-                # - check the mixed precision setting on the FSDP root wrapper.
-                if model.mixed_precision:
-                    for module in model.module():
-                        if isinstance(module, FSDP):
-                            # module.params will hold a list of FlatParameter's
-                            for param in module.params:
-                                if param.dtype != torch.float32 and param.device != torch.device("meta"):
-                                    # TODO: make the warning issue only once
-                                    warnings.warn("Training FSDP with mixed precision. Upcasting parameters to full precision.")
+                # In the event the model had been loaded in low precision, but
+                # mixed precision had also been activated, then we follow DeepSpeed's
+                # strategy to hold the parameters in full precision.
+                # - assume that trainer.args.bf16 and trainer.args.fp16 are already checked against
+                #   fsdp_plugin.mixed_precision_policy.
+                # - NOTE: we do not check the mixed_precision attribute on the FSDP root wrapper.
+                #   * this attribute will always set by init_utils.init_core_state so its always not None.
+                #   * mixed_precision.param_dtype only regards _fwd_bwd_param_dtype
+                #   * if model is loaded in 16bit, and even if mixed_precision.param_dtype is None,
+                #     we sill want to upcast the flat_param.
+                if self.mixed_precision != "no":  # if mixed precision is set
+                    for module in FSDP.fsdp_modules(model):
+                        # Referencing DeepSpeed Zero3
+                        # - in Init, params are converted to 16bit while partitioning.
+                        # - in accelerator.prepare, deepspeed.initalize is called to:
+                        #   * creates the DeepSpeeedEngine.
+                        #   * since zero_optimization() is True , calls engine._configure_zero_optimizer.
+                        #
+                        # Inside the DeepSpeed Zero3 optimizer configuration, which initalizes
+                        # DeepSpeedZeroOptimizer_Stage3, during which:
+                        #   * trainable_param_groups are obtained from the attached optimizer
+                        #     (already partitioned in 16bit).
+                        #   * then _setup_for_real_optimizer -> _create_fp32_partitions
+                        #     which performs the fp32 upcasting.
 
-                                # upcasting to float32 because we are already using mixed precision
-                                # this should be passthrough if dtype already is float32
-                                param.data = param.data.to(torch.float32)
+                        # To mimick DeepSeepds's casting in FSDP, we look at the (single) FlatParameter held
+                        # within an FSDP wrapper. This FlatParameter will be seen by the optimizer.
+                        #  - even though there is a torch.device('meta') guard below, we
+                        #    expect _init_utils._init_param_handle_from_module to already
+                        #    sync the parameter.
+
+                        if not module._has_params:
+                            continue  # skip if FSDP module not managing parameters
+                        param = module._flat_param
+                        if (
+                            param.dtype != torch.float32
+                            and param.device != torch.device("meta")
+                            and param.requires_grad
+                        ):
+                            # TODO: make the warning issue only once
+                            if self.is_main_process:
+                                warnings.warn(
+                                    f"Upcasting low precision parameters in {module.module.__class__.__name__}, "
+                                    "mixed precision is turned on: "
+                                    f"{','.join(module._flat_param._fqns)}."
+                                )
+
+                            # this works because of FSDP's _runtime_utils.lazy_init.
+                            # Have to be careful not to call anything before this that
+                            # triggers lazy_init (e.g., _is_fsdp_root).
+                            param.data = param.data.to(torch.float32)  # upcasting
+                            module._handle._orig_param_dtype = torch.float32  # update
 
                 # if the previous and current models are same, delete the previous one
                 if len(self._models) > 1 and (self._models[-2] is self._models[-1]):
