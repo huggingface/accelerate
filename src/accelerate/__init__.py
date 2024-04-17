e82de1215ae701b6bf567eb705615c656e7f55c7
Zach Mueller <muellerzr@gmail.com>
2024-04-17 11:39:48 -0400

    Release: v0.29.3

--- src/accelerate/__init__.py
+++ src/accelerate/__init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-__version__ = "0.29.2"
+__version__ = "0.29.3"
 
 from .accelerator import Accelerator
 from .big_modeling import (
