a908ee1b36d192f8f259ac260678c8813106694f
Julian Büchel <jub@zurich.ibm.com>
2024-04-24 15:29:46 +0200

    Update src/accelerate/utils/dataclasses.py
    Co-authored-by: Zach Mueller <muellerzr@gmail.com>

--- src/accelerate/utils/dataclasses.py
+++ src/accelerate/utils/dataclasses.py
@@ -166,7 +166,7 @@ class InitProcessGroupKwargs(KwargsHandler):
 
     backend: Optional[str] = "nccl"
     init_method: Optional[str] = None
-    timeout: timedelta = timedelta(seconds=600)
+    timeout: Optional[timedelta] = None
 
 
 # Literals
