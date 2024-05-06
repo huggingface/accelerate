4aa2eaf9c181c862c61e28b2bbc408bb35f61a77
Huazhong Ji <hzji210@gmail.com>
2024-05-06 20:20:22 +0800

    Fixed the problem of incorrect conditional judgment statement when configuring enable_cpu_affinity (#2748)

--- src/accelerate/commands/config/cluster.py
+++ src/accelerate/commands/config/cluster.py
@@ -579,7 +579,7 @@ def get_cluster_input():
 
     # CPU affinity is only supported on NVIDIA hardware for now
     enable_cpu_affinity = False
-    if distributed_type == (DistributedType.NO, DistributedType.MULTI_GPU) and not use_cpu and not use_mps:
+    if distributed_type in (DistributedType.NO, DistributedType.MULTI_GPU) and not use_cpu and not use_mps:
         enable_cpu_affinity = _ask_field(
             "Would you like to enable numa efficiency? (Currently only supported on NVIDIA hardware). [yes/NO]: ",
             _convert_yes_no_to_bool,
