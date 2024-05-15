b7819f5babccc0e4fa5da78f7b0ebe5c31434c47
hkunzhe <hkunzhe24@gmail.com>
2024-05-15 21:07:47 +0800

    use divmod

--- src/accelerate/state.py
+++ src/accelerate/state.py
@@ -437,8 +437,8 @@ class PartialState:
             length = len(inputs[list(inputs.keys())[0]])
             if not all(len(v) == length for v in inputs.values()):
                 raise ValueError("All values in the dictionary must have the same length")
-        num_samples_per_process = length // self.num_processes
-        num_extras = length % self.num_processes
+        
+        num_samples_per_process, num_extras = divmod(length, self.num_processes)
         start_index = self.process_index * num_samples_per_process + min(self.process_index, num_extras)
         end_index = start_index + num_samples_per_process + (1 if self.process_index < num_extras else 0)
 
