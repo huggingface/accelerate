c55e84393e9aacdbd2fb8c343c9ec534d5c44ea1
Marc Sun <marc@huggingface.co>
2024-05-16 11:40:23 +0200

    fix comment

--- tests/test_big_modeling.py
+++ tests/test_big_modeling.py
@@ -484,8 +484,8 @@ class BigModelingTester(unittest.TestCase):
         del model
         gc.collect()
 
-    # This test fails because sometimes data_ptr() of compute2.weight is  the same of compute1.weight.
-    # I check that the values are not the same but it gives the same address. This does not happen on my local machine.
+    # This test fails because sometimes data_ptr() of compute2.weight is the same as compute1.weight.
+    # I checked that the values are not the same but it gives the same address. This does not happen on my local machine.
     @require_cuda
     @unittest.skip(
         "Flaky test, we should have enough coverage with test_dispatch_model_tied_weights_memory_with_nested_offload_cpu test"
