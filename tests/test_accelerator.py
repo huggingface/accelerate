b7fa2fa956f40e0b6f650d5eb1764680bf3fd8f7
Marc Sun <57196510+SunMarc@users.noreply.github.com>
2024-06-03 14:37:44 +0200

    add cuda dep for a test (#2820)
    * add cuda dep for a test
    
    * hmmm

--- tests/test_accelerator.py
+++ tests/test_accelerator.py
@@ -282,6 +282,7 @@ class AcceleratorTester(AccelerateTestCase):
         assert torch.allclose(expected, output, atol=1e-5)
 
     @parameterized.expand([True, False], name_func=parameterized_custom_name_func)
+    @require_cuda
     def test_get_state_dict_from_offload(self, use_safetensors):
         accelerator = Accelerator()
 
