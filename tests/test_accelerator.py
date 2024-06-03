0bbee65ae8354e5a7b6387851e277c7b077781a8
Marc Sun <marc@huggingface.co>
2024-06-03 14:22:59 +0200

    hmmm

--- tests/test_accelerator.py
+++ tests/test_accelerator.py
@@ -281,8 +281,8 @@ class AcceleratorTester(AccelerateTestCase):
             output = model(inputs)
         assert torch.allclose(expected, output, atol=1e-5)
 
-    @require_cuda
     @parameterized.expand([True, False], name_func=parameterized_custom_name_func)
+    @require_cuda
     def test_get_state_dict_from_offload(self, use_safetensors):
         accelerator = Accelerator()
 
