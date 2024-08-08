79ca85c27df292dbf64cfa2bcc12dbb62fbe9267
Yitong Huang <huangyitong.hyt@alibaba-inc.com>
2024-08-08 20:55:44 +0800

    Support skip_first_batches for XLA (#2966)
    * Fix skip_first_batches for XLA
    
    * Use state to check XLA
    
    * Change to PartialState

--- src/accelerate/data_loader.py
+++ src/accelerate/data_loader.py
@@ -20,7 +20,7 @@ import torch
 from torch.utils.data import BatchSampler, DataLoader, IterableDataset, RandomSampler
 
 from .logging import get_logger
-from .state import AcceleratorState, DistributedType, GradientState, is_torch_xla_available
+from .state import AcceleratorState, DistributedType, GradientState, PartialState, is_torch_xla_available
 from .utils import (
     RNGType,
     broadcast,
@@ -539,6 +539,7 @@ if is_torch_xla_available():
             super().__init__(dataloader, device)
             self._rng_types = self._loader.rng_types
             self._loader.rng_types = None
+            self.device = device
 
         def __iter__(self):
             if self._rng_types is not None:
@@ -557,6 +558,10 @@ if is_torch_xla_available():
         @property
         def batch_sampler(self):
             return self._loader.batch_sampler
+
+        @property
+        def dataloader(self):
+            return self._loader
 
 
 class DataLoaderDispatcher(DataLoader, DataLoaderStateMixin):
@@ -1083,6 +1088,11 @@ def skip_first_batches(dataloader, num_batches=0):
     """
     Creates a `torch.utils.data.DataLoader` that will efficiently skip the first `num_batches`.
     """
+    state = PartialState()
+    if state.distributed_type == DistributedType.XLA:
+        device = dataloader.device
+        dataloader = dataloader.dataloader
+
     dataset = dataloader.dataset
     sampler_is_batch_sampler = False
     if isinstance(dataset, IterableDataset):
@@ -1145,5 +1155,8 @@ def skip_first_batches(dataloader, num_batches=0):
             dataloader = SkipDataLoader(dataset, skip_batches=num_batches, **kwargs)
         else:
             dataloader = DataLoader(dataset, batch_sampler=new_batch_sampler, **kwargs)
+
+    if state.distributed_type == DistributedType.XLA:
+        dataloader = MpDeviceLoaderWrapper(dataloader, device)
 
     return dataloader
