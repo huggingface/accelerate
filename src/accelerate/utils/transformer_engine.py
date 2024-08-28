942fbe40d6cb4fef0c430ae5d04093c81f5e573f
Yichen Yan <oraluben@outlook.com>
2024-08-28 19:22:36 +0800

    resolve comment 1/2

--- src/accelerate/utils/transformer_engine.py
+++ src/accelerate/utils/transformer_engine.py
@@ -20,22 +20,15 @@ import torch.nn as nn
 from .imports import is_fp8_available
 from .operations import GatheredParameters
 
-
-def require_te(fn):
-    if not is_fp8_available():
-        @wraps(fn)
-        def i(*args, **kwargs):
-            raise ImportError(f"Using `{fn.__name__}` requires transformer_engine to be installed.")
-        return i
-
-    return fn
+# Do not import `transformer_engine` at package level to avoid potential issues
 
 
-@require_te
 def convert_model(model, to_transformer_engine=True, _convert_linear=True, _convert_ln=True):
     """
     Recursively converts the linear and layernorm layers of a model to their `transformers_engine` counterpart.
     """
+    if not is_fp8_available():
+        raise ImportError("Using `convert_model` requires transformer_engine to be installed.")
     import transformer_engine.pytorch as te
 
     for name, module in model.named_children():
@@ -89,11 +82,12 @@ def convert_model(model, to_transformer_engine=True, _convert_linear=True, _conv
             )
 
 
-@require_te
 def has_transformer_engine_layers(model):
     """
     Returns whether a given model has some `transformer_engine` layer or not.
     """
+    if not is_fp8_available():
+        raise ImportError("Using `convert_model` requires transformer_engine to be installed.")
     import transformer_engine.pytorch as te
 
     for m in model.modules():
@@ -102,12 +96,13 @@ def has_transformer_engine_layers(model):
     return False
 
 
-@require_te
 def contextual_fp8_autocast(model_forward, fp8_recipe, use_during_eval=False):
     """
     Wrapper for a model's forward method to apply FP8 autocast. Is context aware, meaning that by default it will
     disable FP8 autocast during eval mode, which is generally better for more accurate metrics.
     """
+    if not is_fp8_available():
+        raise ImportError("Using `convert_model` requires transformer_engine to be installed.")
     from transformer_engine.pytorch import fp8_autocast
 
     def forward(self, *args, **kwargs):
@@ -121,11 +116,12 @@ def contextual_fp8_autocast(model_forward, fp8_recipe, use_during_eval=False):
     return forward
 
 
-@require_te
 def apply_fp8_autowrap(model, fp8_recipe_handler):
     """
     Applies FP8 context manager to the model's forward method
     """
+    if not is_fp8_available():
+        raise ImportError("Using `convert_model` requires transformer_engine to be installed.")
     import transformer_engine.common.recipe as te_recipe
 
     kwargs = fp8_recipe_handler.to_kwargs() if fp8_recipe_handler is not None else {}
