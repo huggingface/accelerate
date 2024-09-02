b07ad2adf29f6777fa94db453eb6b963f891a538
Moki <38482240+zmoki688@users.noreply.github.com>
2024-09-02 23:47:04 +0800

    Fix typo in comment (#3045)
    * Fix typo in comment
    
    * Fix typo in comment: quality check

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -166,8 +166,8 @@ class Accelerator:
             Whether or not the accelerator should put objects on device (tensors yielded by the dataloader, model,
             etc...).
         mixed_precision (`str`, *optional*):
-            Whether or not to use mixed precision training. Choose from 'no','fp16','bf16 or 'fp8'. Will default to the
-            value in the environment variable `ACCELERATE_MIXED_PRECISION`, which will use the default value in the
+            Whether or not to use mixed precision training. Choose from 'no','fp16','bf16' or 'fp8'. Will default to
+            the value in the environment variable `ACCELERATE_MIXED_PRECISION`, which will use the default value in the
             accelerate config of the current system or the flag passed with the `accelerate.launch` command. 'fp8'
             requires the installation of transformers-engine.
         gradient_accumulation_steps (`int`, *optional*, default to 1):
