40ec962a62da8fa2d82bc20a1979f24c202b5895
byi8220 <byi8220@gmail.com>
2024-09-03 20:35:43 -0400

    pickling generator issues

--- src/accelerate/test_utils/scripts/test_distributed_data_loop.py
+++ src/accelerate/test_utils/scripts/test_distributed_data_loop.py
@@ -74,7 +74,7 @@ class DummyIterableDataset(IterableDataset):
 def create_accelerator(even_batches=True):
     dataloader_config = DataLoaderConfiguration(even_batches=even_batches)
     accelerator = Accelerator(dataloader_config=dataloader_config)
-    # assert accelerator.num_processes == 2, "this script expects that two GPUs are available"
+    assert accelerator.num_processes == 2, "this script expects that two GPUs are available"
     return accelerator
 
 
@@ -341,8 +341,6 @@ def test_stateful_dataloader_save_state(accelerator):
         accelerator.dataloader_config = old_dataloader_config
 
 def test_pickled_dataloader(data_loader, accelerator):
-    # Prepare the DataLoader
-    data_loader = accelerator.prepare(data_loader)
     # Pickle then reload the dataloader
     prepared_model_dumps = pickle.dumps(accelerator)
     loaded_accelerator = pickle.loads(prepared_model_dumps)
