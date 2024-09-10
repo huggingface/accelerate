e845f429d2612ddc50f49eb88b8d18a033f802d3
[[ -z $EMAIL ]] && read -e -p "Enter your email (for git configuration): " EMAIL <muellerzr@gmail.com>
2024-09-10 11:04:29 -0400

    New ruff version cleaning

--- benchmarks/fp8/ms_amp/ddp.py
+++ benchmarks/fp8/ms_amp/ddp.py
@@ -17,6 +17,7 @@ This script tests to ensure that `accelerate` performs at the same level as raw 
 
 This particular script verifies this for DDP training.
 """
+
 import evaluate
 import msamp
 import torch
