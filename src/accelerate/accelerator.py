729a56a0745fb291fdd1e54056a059ba309e3bd7
Rebornix-zero <1980250928@qq.com>
2024-10-22 10:09:04 +0800

    docs: fix a wrong word in src/accelerate/accelerate.py:1255

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -1252,7 +1252,7 @@ class Accelerator:
         >>> accelerator = Accelerator()
         >>> # Assume a model, optimizer, data_loader and scheduler are defined
         >>> device_placement = [True, True, False, False]
-        >>> # Will place the first to items passed in automatically to the right device but not the last two.
+        >>> # Will place the first two items passed in automatically to the right device but not the last two.
         >>> model, optimizer, data_loader, scheduler = accelerator.prepare(
         ...     model, optimizer, data_loader, scheduler, device_placement=device_placement
         ... )
