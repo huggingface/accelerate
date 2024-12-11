fafbcb3a90989ec887eab7aa55465c5766e1c57a
[[ -z $EMAIL ]] && read -e -p "Enter your email (for git configuration): " EMAIL <muellerzr@gmail.com>
2024-12-10 23:26:51 -0500

    Purposefully fail quality to test error messages

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import annotations
+from __future__ import (
+    annotations
+)
 
 import contextlib
 import functools
