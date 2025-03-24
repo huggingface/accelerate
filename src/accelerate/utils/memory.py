94482ba94d9bd49836e3f85bd8f950088ba7e0a9
Cameron Shinn <camerontshinn@gmail.com>
2025-03-24 10:37:48 -0700

    Update memory.py
    Consolidate OOM error check string

--- src/accelerate/utils/memory.py
+++ src/accelerate/utils/memory.py
@@ -99,9 +99,7 @@ def should_reduce_batch_size(exception: Exception) -> bool:
             An exception
     """
     _statements = [
-        "CUDA out of memory.",  # CUDA OOM
-        "HIP out of memory.",  # HIP OOM
-        "XPU out of memory.",  # XPU OOM
+        " out of memory.",  # OOM for CUDA, HIP, XPU
         "cuDNN error: CUDNN_STATUS_NOT_SUPPORTED.",  # CUDNN SNAFU
         "DefaultCPUAllocator: can't allocate memory",  # CPU OOM
     ]
