5dbea455f3ed5b06848ae35a83420193d4016a43
[[ -z $EMAIL ]] && read -e -p "Enter your email (for git configuration): " EMAIL <muellerzr@gmail.com>
2025-04-04 12:28:49 -0400

    ao in setup too

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ extras["testing"] = extras["test_prod"] + extras["test_dev"]
 extras["deepspeed"] = ["deepspeed"]
 extras["rich"] = ["rich"]
 
-extras["test_fp8"] = ["torchao"]
+extras["test_fp8"] = ["torchao"] # note: TE for now needs to be done via pulling down the docker image directly
 extras["test_trackers"] = ["wandb", "comet-ml", "tensorboard", "dvclive", "mlflow", "matplotlib"]
 extras["dev"] = extras["quality"] + extras["testing"] + extras["rich"]
 
