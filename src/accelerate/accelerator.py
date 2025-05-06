e2442072d174de0170885556d51cfdf5dd6fb333
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2025-05-06 15:07:33 +0200

    support deepspeed cases

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -2030,7 +2030,10 @@ class Accelerator:
 
             if compare_versions("deepspeed", ">=", "0.14.4") and self.state.dynamo_plugin.backend != DynamoBackend.NO:
                 compile_kwargs = self.state.dynamo_plugin.to_kwargs()
-                engine.compile(backend=compile_kwargs.pop("backend"), compile_kwargs=compile_kwargs)
+                if self.state.dynamo_plugin.use_regional_compilation:
+                    engine.module = compile_regions(engine.module, **compile_kwargs)
+                else:
+                    engine.compile(backend=compile_kwargs.pop("backend"), compile_kwargs=compile_kwargs)
             if optimizer is not None:
                 optimizer = DeepSpeedOptimizerWrapper(optimizer)
             if scheduler is not None:
