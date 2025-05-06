af7304cec321f32863aed11f7833f1d860b9e094
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2025-05-06 14:27:01 +0200

    move to training

--- src/accelerate/utils/other.py
+++ src/accelerate/utils/other.py
@@ -80,9 +80,9 @@ def compile_regions(module: torch.nn.Module, **compile_kwargs) -> torch.nn.Modul
     Performs a version of the regional compilation recipe where we target repeated blocks of the same class and compile
     them sequentially to hit the compiler's cache. For example, in `GPT2LMHeadModel`, the repeated block is `GPT2Block`,
     and can be accessed as `model.transformer.h[0]`.
-    This allows us to speed up the compilation time / cold start of models like LLMs and Transformers in general.
 
-    See: https://pytorch.org/tutorials/recipes/regional_compilation.html
+    This allows us to speed up the compilation overhead / cold start of models like LLMs and Transformers in general.
+    See https://pytorch.org/tutorials/recipes/regional_compilation.html for more details.
 
     Args:
         module (`torch.nn.Module`):
