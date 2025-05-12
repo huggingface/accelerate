ddc49f1e9a8fdfc217c2c961969d2db6955c82df
Jixiang Hong <33440225+hongjx175@users.noreply.github.com>
2025-05-12 20:30:19 +0800

    Fix the issue where `set_epoch` does not take effect. (#3556)
    * Fix the issue where `set_epoch` does not take effect.
    
    * Apply style fixes
    
    ---------
    
    Co-authored-by: root <root@hjx-dev-h20-3-0.hjx-dev-h20-3.bcloud.svc.cluster.local>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/accelerate/data_loader.py
+++ src/accelerate/data_loader.py
@@ -606,6 +606,12 @@ class DataLoaderShard(DataLoaderAdapter, DataLoaderStateMixin):
             self.batch_sampler.set_epoch(epoch)
         if hasattr(self.batch_sampler, "sampler") and hasattr(self.batch_sampler.sampler, "set_epoch"):
             self.batch_sampler.sampler.set_epoch(epoch)
+        if (
+            hasattr(self.batch_sampler, "batch_sampler")
+            and hasattr(self.batch_sampler.batch_sampler, "sampler")
+            and hasattr(self.batch_sampler.batch_sampler.sampler, "set_epoch")
+        ):
+            self.batch_sampler.batch_sampler.sampler.set_epoch(epoch)
         # We support if a custom `Dataset` implementation has `set_epoch`
         # or in general HF datasets `Datasets`
         elif hasattr(self.dataset, "set_epoch"):
