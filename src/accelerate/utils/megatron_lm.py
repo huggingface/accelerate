6dbe2c47b1d53232972aeab370a67fecb71aaec2
pengdurice <pengduhit@gmail.com>
2025-11-18 17:34:39 +0000

    save before updating to a new branch

--- src/accelerate/utils/megatron_lm.py
+++ src/accelerate/utils/megatron_lm.py
@@ -122,19 +122,19 @@ def model_provider_func(pre_process=True, post_process=True, add_encoder=True, a
     args.recompute_method = "uniform"
     args.recompute_num_layers = 1
     # args.use_torch_fsdp2 = True
-    args.optimizer_cpu_offload = True
-    args.overlap_cpu_optimizer_d2h_h2d = True
+    # args.optimizer_cpu_offload = True
+    # args.overlap_cpu_optimizer_d2h_h2d = True
 
     args.use_custom_fsdp = True
     args.sequence_parallel = True
     args.attention_backend = True
-    args.expert_model_parallel_size = 16
+    args.expert_model_parallel_size = 1
     args.context_parallel_size = 2
     args.attention_dropout = 0.0
     args.hidden_dropout = 0.0
     args.weight_decay = 0.1
     args.attention_softmax_in_fp32 = True
-    args.expert_tensor_parallel_size = 16
+    args.expert_tensor_parallel_size = 1
     args.calculate_per_token_loss = True
     args.use_rotary_position_embeddings = True
     # args.data_parallel_sharding_strategy = "optim_grads_params"
