4111a9692d2e8d616101a165d2961761e233dbc1
Reza Qorbani <rezaqorbani@Rezas-MacBook-Pro.local>
2025-12-27 23:02:14 +0200

    Fix hf_device_map device comparison in prepare_model

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -1811,10 +1811,11 @@ class Accelerator:
                 else:
                     current_device_index = current_device
 
+                current_device_index = int(current_device_index) if current_device_index is not None else None
                 if self.device.type == "cpu" and is_bitsandbytes_multi_backend_available():
                     # bnb with multi-backend supports CPU which don't need to check index.
                     pass
-                elif torch.device(current_device_index) != self.device:
+                elif torch.device(self.device.type, current_device_index) != self.device:
                     # if on the first device (GPU 0) we don't care
                     if (self.device.index is not None) or (current_device_index != 0):
                         raise ValueError(
