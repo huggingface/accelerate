67ac44eeb5eb25a560a8e50da0e2323ff8baf57b
Marc Sun <marc@huggingface.co>
2026-03-04 18:05:23 +0000

    fix bnb

--- src/accelerate/utils/bnb.py
+++ src/accelerate/utils/bnb.py
@@ -133,16 +133,11 @@ def load_and_quantize_model(
         model = replace_with_bnb_layers(model, bnb_quantization_config, modules_to_not_convert=modules_to_not_convert)
         # convert param to the right dtype
         dtype = bnb_quantization_config.torch_dtype
-        for name, param in model.state_dict().items():
+        for name, param in model.named_parameters():
             if any(module_to_keep_in_fp32 in name for module_to_keep_in_fp32 in keep_in_fp32_modules):
-                param.to(torch.float32)
-                if param.dtype != torch.float32:
-                    name = name.replace(".weight", "").replace(".bias", "")
-                    param = getattr(model, name, None)
-                    if param is not None:
-                        param.to(torch.float32)
+                param.data = param.data.to(torch.float32)
             elif torch.is_floating_point(param):
-                param.to(dtype)
+                param.data = param.data.to(dtype)
         if model_device.type == "cuda":
             model.cuda(torch.cuda.current_device())
             torch.cuda.empty_cache()
