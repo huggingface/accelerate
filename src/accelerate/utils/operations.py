6b548691f8b7a988b886a0d22955613198bd06f5
Yuanyuan Chen <cyyever@outlook.com>
2026-03-14 15:37:53 +0800

    Remove is_torch_xpu_tensor
    Signed-off-by: Yuanyuan Chen <cyyever@outlook.com>

--- src/accelerate/utils/operations.py
+++ src/accelerate/utils/operations.py
@@ -46,18 +46,6 @@ def is_torch_tensor(tensor):
     return isinstance(tensor, torch.Tensor)
 
 
-def is_torch_xpu_tensor(tensor):
-    return isinstance(
-        tensor,
-        torch.xpu.FloatTensor,
-        torch.xpu.ByteTensor,
-        torch.xpu.IntTensor,
-        torch.xpu.LongTensor,
-        torch.xpu.HalfTensor,
-        torch.xpu.DoubleTensor,
-        torch.xpu.BFloat16Tensor,
-    )
-
 
 def is_tensor_information(tensor_info):
     return isinstance(tensor_info, TensorInformation)
