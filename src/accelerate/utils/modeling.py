81b2681a850181fa313f1482b740b3e1be131ca0
Marc Sun <marc@huggingface.co>
2026-03-16 17:44:24 +0000

    fix-bnb

--- src/accelerate/utils/modeling.py
+++ src/accelerate/utils/modeling.py
@@ -349,7 +349,7 @@ def set_module_tensor_to_device(
             module._buffers[tensor_name] = new_value
         elif value is not None or not check_device_same(torch.device(device), module._parameters[tensor_name].device):
             param_cls = type(module._parameters[tensor_name])
-            kwargs = module._parameters[tensor_name].__dict__
+            kwargs = {k: v for k, v in module._parameters[tensor_name].__dict__.items() if k != "_is_hf_initialized"}
             if param_cls.__name__ in ["Int8Params", "FP4Params", "Params4bit"]:
                 if param_cls.__name__ == "Int8Params" and new_value.dtype == torch.float32:
                     # downcast to fp16 if any - needed for 8bit serialization
