c2b15b4e948f1e6b86ba299c7bb9610e23db4573
Kyle Sayers <kylesayrs@gmail.com>
2026-06-18 14:40:18 -0400

    add comment
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- tests/test_big_modeling.py
+++ tests/test_big_modeling.py
@@ -780,6 +780,7 @@ class BigModelingTester(unittest.TestCase):
     def test_dispatch_model_disk_only(self):
         model = ModelForTest()
 
+        # note: non-expanded device maps for disk offloading is not supported
         device_map = {"linear1": "disk", "batchnorm": "disk", "linear2": "disk"}
 
         x = torch.randn(2, 3)
