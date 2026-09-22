2c2c1fb003d0775b0f865a6226ce76a2886f435b
Zhiqi Zhang <zhiqizhangg@gmail.com>
2026-09-22 02:10:53 +0000

    Fix split_between_processes on tuples with apply_padding=True

--- src/accelerate/state.py
+++ src/accelerate/state.py
@@ -487,7 +487,11 @@ class PartialState:
                             tensorized_result, pad_index=send_to_device(inputs[-1], self.device)
                         )
                     else:
-                        result += [inputs[-1]] * (num_samples_per_process + (1 if num_extras > 0 else 0) - len(result))
+                        num_padding = num_samples_per_process + (1 if num_extras > 0 else 0) - len(result)
+                        if isinstance(result, tuple):
+                            result += (inputs[-1],) * num_padding
+                        else:
+                            result += [inputs[-1]] * num_padding
                 return result
             elif isinstance(inputs, dict):
                 for key in inputs.keys():
