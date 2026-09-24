95979d80ed18cfc3284c60d0f62f5ba3484cace8
jiqing-feng <jiqing.feng@intel.com>
2026-09-24 18:21:30 +0800

    Condense the tied-weight comments in the onload/offload paths

--- src/accelerate/hooks.py
+++ src/accelerate/hooks.py
@@ -368,9 +368,7 @@ class AlignDevicesHook(ModelHook):
             value = self.weights_map[name]
             fp16_statistics = self._maybe_get_fp16_statistics(name, value)
 
-            # In case we are using offloading with tied weights, we need to keep track of the offloaded weights
-            # that are loaded on device at this point, as we will need to remove them as well from the dictionary
-            # self.tied_params_map in order to allow to free memory.
+            # Track onloaded tied weights so _offload_weights can drop them from tied_params_map.
             if name in self.tied_params_names and value.data_ptr() not in self.tied_params_map:
                 self.tied_params_map[value.data_ptr()] = {}
 
@@ -404,8 +402,7 @@ class AlignDevicesHook(ModelHook):
                 module.state.SCB = None
                 module.state.CxB = None
 
-        # We may have loaded tied weights into self.tied_params_map (avoiding to load them several times in e.g. submodules): remove them from
-        # this dictionary to allow the garbage collector to do its job.
+        # Drop the tied weights onloaded above from tied_params_map so they can be freed.
         for value_pointer, device in self.tied_pointers_to_remove:
             if isinstance(device, int):
                 if is_npu_available():
